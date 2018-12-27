ac4cc338fdbfdd3a8f6a5d319e0c5d431256b31e
Ferdinand <fexcalo@gmail.com>
2018-12-27 21:31:25 +0100

    tmt 1.3

--- src/main/java/net/fexcraft/lib/mc/TMT.java
+++ src/main/java/net/fexcraft/lib/mc/TMT.java
@@ -16,7 +16,7 @@ public class TMT {
 	@Mod.Instance("tmt")
 	private static TMT INSTANCE;
 	public static final String MODID = "tmt";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	
 	@Mod.EventHandler
 	public void preinit(FMLPreInitializationEvent event){
