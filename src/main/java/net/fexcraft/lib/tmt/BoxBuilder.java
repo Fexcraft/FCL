a817c633bfb8dfb78339d5e6eab7e0f4bba3ca5d
Ferdinand <fercalo96@yahoo.de>
2020-08-02 09:29:40 +0200

    Update BoxBuilder.java

--- src/main/java/net/fexcraft/lib/tmt/BoxBuilder.java
+++ src/main/java/net/fexcraft/lib/tmt/BoxBuilder.java
@@ -201,9 +201,9 @@ public class BoxBuilder {
         verts[4] = tv4; verts[5] = tv5; verts[6] = tv6; verts[7] = tv7;
         float tx = root.texoffx, ty = root.texoffy;
         float w = this.w, h = this.h, d = this.d;
-        if(w % 1 != 0) w = (int)w + (w % 1 > 0.5f ? 1 : 0);
-        if(h % 1 != 0) h = (int)h + (h % 1 > 0.5f ? 1 : 0);
-        if(d % 1 != 0) d = (int)d + (d % 1 > 0.5f ? 1 : 0);
+        if(w % 1 != 0) w = w < 1 ? 1 : (int)w + (w % 1 > 0.5f ? 1 : 0);
+        if(h % 1 != 0) h = h < 1 ? 1 : (int)h + (h % 1 > 0.5f ? 1 : 0);
+        if(d % 1 != 0) d = d < 1 ? 1 : (int)d + (d % 1 > 0.5f ? 1 : 0);
         if(!anyoff){
             poly[0] = genPolygonWithUV(0, new TexturedVertex[] { tv5, tv1, tv2, tv6 }, tx, ty, d + w, d, d, h);
             poly[1] = genPolygonWithUV(1, new TexturedVertex[] { tv0, tv4, tv7, tv3 }, tx, ty, 0, d, d, h);
