72743ca2ccea23ac414a480d1b5b0027adff782b
Ferdinand <fercalo96@yahoo.de>
2021-01-12 19:22:07 +0100

    Quad Shading Fix.

--- src/main/java/net/fexcraft/lib/common/math/TexturedPolygon.java
+++ src/main/java/net/fexcraft/lib/common/math/TexturedPolygon.java
@@ -49,46 +49,96 @@ public class TexturedPolygon {
 		else{
 	        switch(vertices.length){
 		        case 3: GL11.glBegin(GL11.GL_TRIANGLES); break;
-		        case 4: GL11.glBegin(GL11.GL_QUADS); break;
+		        case 4: GL11.glBegin(GL11.GL_TRIANGLES/*GL11.GL_QUADS*/); break;
 		        default: GL11.glBegin(GL11.GL_POLYGON); break;
 	        };
 		}
 		boolean gnorm = list.isEmpty() || list.size() != vertices.length;
-        if(gnorm){
-	        if(normals.length == 3){
-	        	if(invert){ GL11.glNormal3f(-normals[0], -normals[1], -normals[2]); }
-	        	else{ GL11.glNormal3f(normals[0], normals[1], normals[2]); }
-	        }
-	        else if(vertices.length >= 3){
-		        Vec3f vec0 = new Vec3f(vertices[1].vector.subtract(vertices[0].vector));
-		        Vec3f vec1 = new Vec3f(vertices[1].vector.subtract(vertices[2].vector));
-		        Vec3f vec2 = vec1.crossProduct(vec0).normalize();
-		        normals = new float[]{ vec2.xCoord, vec2.yCoord, vec2.zCoord };
-		        if(invert){ GL11.glNormal3f(-vec2.xCoord, -vec2.yCoord, -vec2.zCoord); }
-		        else{ GL11.glNormal3f(vec2.xCoord, vec2.yCoord, vec2.zCoord); }
-	        }
-	        else return;
-        }
-        for(int i = 0; i < vertices.length; i++){
-        	TexturedVertex texvex = vertices[i];
-        	if(!gnorm){
-            	Vec3f norm = list.get(i);
-            	if(invert) GL11.glNormal3f(-norm.xCoord, -norm.yCoord, -norm.zCoord);
-            	else GL11.glNormal3f(norm.xCoord, norm.yCoord, norm.zCoord);
+        if(gnorm) checkGenerated();
+        if(vertices.length == 4){
+        	if(gnorm){
+        		if(invert){ GL11.glNormal3f(-normals[0], -normals[1], -normals[2]); }
+            	else{ GL11.glNormal3f(normals[0], normals[1], normals[2]); }
         	}
             if(rgb == null && color == null){
-            	GL11.glTexCoord2f(texvex.textureX, texvex.textureY);
-            	GL11.glVertex3f(texvex.vector.xCoord * scale, texvex.vector.yCoord * scale, texvex.vector.zCoord * scale);
+            	triangleT(scale, 0, 1, 2);
             }
             else{
             	(color == null ? rgb : color).glColorApply();
-            	GL11.glVertex3f(texvex.vector.xCoord * scale, texvex.vector.yCoord * scale, texvex.vector.zCoord * scale);
+            	triangleT(scale, 0, 1, 2);
+            }
+        	if(gnorm){
+        		if(invert){ GL11.glNormal3f(-normals[3], -normals[4], -normals[5]); }
+            	else{ GL11.glNormal3f(normals[3], normals[4], normals[5]); }
+        	}
+            if(rgb == null && color == null){
+            	triangleT(scale, 0, 2, 3);
+            }
+            else{
+            	(color == null ? rgb : color).glColorApply();
+            	triangleC(scale, 0, 2, 3);
+            }
+        }
+        else{
+            for(int i = 0; i < vertices.length; i++){
+            	TexturedVertex texvex = vertices[i];
+            	if(!gnorm){
+                	Vec3f norm = list.get(i);
+                	if(invert) GL11.glNormal3f(-norm.xCoord, -norm.yCoord, -norm.zCoord);
+                	else GL11.glNormal3f(norm.xCoord, norm.yCoord, norm.zCoord);
+            	}
+            	else{
+                	if(invert){ GL11.glNormal3f(-normals[0], -normals[1], -normals[2]); }
+                	else{ GL11.glNormal3f(normals[0], normals[1], normals[2]); }
+            	}
+                if(rgb == null && color == null){
+                	GL11.glTexCoord2f(texvex.textureX, texvex.textureY);
+                	GL11.glVertex3f(texvex.vector.xCoord * scale, texvex.vector.yCoord * scale, texvex.vector.zCoord * scale);
+                }
+                else{
+                	(color == null ? rgb : color).glColorApply();
+                	GL11.glVertex3f(texvex.vector.xCoord * scale, texvex.vector.yCoord * scale, texvex.vector.zCoord * scale);
+                }
             }
         }
         GL11.glEnd();
     }
 
-    public void flipFace(){
+    private void triangleT(float scale, int x, int y, int z){
+    	GL11.glTexCoord2f(vertices[x].textureX, vertices[x].textureY);
+    	GL11.glVertex3f(vertices[x].vector.xCoord * scale, vertices[x].vector.yCoord * scale, vertices[x].vector.zCoord * scale);
+    	GL11.glTexCoord2f(vertices[y].textureX, vertices[y].textureY);
+    	GL11.glVertex3f(vertices[y].vector.xCoord * scale, vertices[y].vector.yCoord * scale, vertices[y].vector.zCoord * scale);
+    	GL11.glTexCoord2f(vertices[z].textureX, vertices[z].textureY);
+    	GL11.glVertex3f(vertices[z].vector.xCoord * scale, vertices[z].vector.yCoord * scale, vertices[z].vector.zCoord * scale);
+	}
+    
+    private void triangleC(float scale, int x, int y, int z){
+    	GL11.glVertex3f(vertices[x].vector.xCoord * scale, vertices[x].vector.yCoord * scale, vertices[x].vector.zCoord * scale);
+    	GL11.glVertex3f(vertices[y].vector.xCoord * scale, vertices[y].vector.yCoord * scale, vertices[y].vector.zCoord * scale);
+    	GL11.glVertex3f(vertices[z].vector.xCoord * scale, vertices[z].vector.yCoord * scale, vertices[z].vector.zCoord * scale);
+	}
+
+	private void checkGenerated(){
+    	if(normals.length >= 3) return;
+        if(vertices.length == 4){
+	        Vec3f vec0 = new Vec3f(vertices[1].vector.subtract(vertices[0].vector));
+	        Vec3f vec1 = new Vec3f(vertices[1].vector.subtract(vertices[2].vector));
+	        Vec3f vec2 = vec1.crossProduct(vec0).normalize();
+	        vec0 = new Vec3f(vertices[2].vector.subtract(vertices[0].vector));
+	        vec1 = new Vec3f(vertices[2].vector.subtract(vertices[3].vector));
+	        Vec3f vec3 = vec1.crossProduct(vec0).normalize();
+	        normals = new float[]{ vec2.xCoord, vec2.yCoord, vec2.zCoord, vec3.xCoord, vec3.yCoord, vec3.zCoord };
+        }
+		else if(vertices.length >= 3){
+	        Vec3f vec0 = new Vec3f(vertices[1].vector.subtract(vertices[0].vector));
+	        Vec3f vec1 = new Vec3f(vertices[1].vector.subtract(vertices[2].vector));
+	        Vec3f vec2 = vec1.crossProduct(vec0).normalize();
+	        normals = new float[]{ vec2.xCoord, vec2.yCoord, vec2.zCoord };
+        }
+	}
+
+	public void flipFace(){
         TexturedVertex[] verts = new TexturedVertex[vertices.length];
         for(int i = 0; i < vertices.length; ++i){
             verts[i] = vertices[vertices.length - i - 1];
