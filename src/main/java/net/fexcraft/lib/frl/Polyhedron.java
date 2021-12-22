d390c5acc97e27ab9f85ede9552b005ff6321679
Ferdinand Calo <fercalo96@yahoo.de>
2021-12-22 20:51:05 +0100

    Update Polyhedron.java

--- src/main/java/net/fexcraft/lib/frl/Polyhedron.java
+++ src/main/java/net/fexcraft/lib/frl/Polyhedron.java
@@ -5,6 +5,9 @@ import static net.fexcraft.lib.frl.Renderer.RENDERER;
 import java.util.ArrayList;
 
 import net.fexcraft.lib.common.math.RGB;
+import net.fexcraft.lib.common.math.TexturedPolygon;
+import net.fexcraft.lib.common.math.Vec3f;
+import net.fexcraft.lib.tmt.ModelRendererTurbo;
 
 /**
  * 
@@ -44,6 +47,30 @@ public class Polyhedron<GLO> {
 
 	public void render(){
 		RENDERER.render(this);
+	}
+	
+	public Polyhedron<GLO> importMRT(ModelRendererTurbo turbo, float scale){
+		this.name = turbo.boxName;
+		for(TexturedPolygon tp : turbo.getFaces()){
+			Vertex[] verts = new Vertex[tp.getVertices().length];
+			for(int i = 0; i < verts.length; i++){
+				verts[i] = new ColoredVertex(new Vec3f(tp.getVertices()[i].vector), tp.getVertices()[i].textureX, tp.getVertices()[i].textureY);
+		        Vec3f vec0 = new Vec3f(tp.getVertices()[1].vector.sub(tp.getVertices()[0].vector));
+		        Vec3f vec1 = new Vec3f(tp.getVertices()[1].vector.sub(tp.getVertices()[2].vector));
+		        Vec3f vec2 = vec1.cross(vec0).normalize();
+				verts[i].norm(vec2);
+				//verts[i].color(1, i == 2 || i == 3 ? 1 : 0, 0);
+			}
+			polygons.add(new Polygon(verts));//.colored(true));
+		}
+		posX = turbo.rotationPointX;
+		posY = turbo.rotationPointY;
+		posZ = turbo.rotationPointZ;
+		rotX = turbo.rotationAngleX;
+		rotY = turbo.rotationAngleY;
+		rotZ = turbo.rotationAngleZ;
+		if(scale != 0f && scale != 1f) rescale(scale);
+		return this;
 	}
 
 }
