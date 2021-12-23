2bc0b23accfe76b843a0cf5956662722d08a9a0a
Ferdinand Calo <fercalo96@yahoo.de>
2021-12-23 20:02:10 +0100

    Update Generator.java

--- src/main/java/net/fexcraft/lib/frl/gen/Generator.java
+++ src/main/java/net/fexcraft/lib/frl/gen/Generator.java
@@ -1,5 +1,6 @@
 package net.fexcraft.lib.frl.gen;
 
+import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.frl.Polyhedron;
 
 /**
@@ -8,12 +9,39 @@ import net.fexcraft.lib.frl.Polyhedron;
  *
  * @param <GLO>
  */
-public interface Generator<GLO> {
+public abstract class Generator<GLO> {
+	
+	protected Polyhedron<GLO> poly;
+	protected JsonMap map;
+	
+	public Generator(Polyhedron<GLO> poly){
+		this.poly = poly;
+	}
 
-	public Polyhedron<GLO> get();
+	public Polyhedron<GLO> get(){
+		return poly;
+	}
 	
-	public Polyhedron<GLO> make();
+	public abstract Polyhedron<GLO> make();
 	
-	public Generator<GLO> removePolygon(int index);
+	public Generator<GLO> removePolygon(int index){
+		if(!map.has("rem_poly")) map.addArray("rem_poly");
+		map.getArray("rem_poly").add(index);
+		return this;
+	}
+	
+	public Generator<GLO> removePolygon(int... idxs){
+		for(int i : idxs) removePolygon(i);
+		return this;
+	}
+
+	public Generator<GLO> setMap(JsonMap map){
+		this.map = map;
+		return this;
+	}
+
+	public JsonMap getMap(){
+		return map;
+	}
 
 }
