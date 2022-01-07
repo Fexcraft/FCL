25b31d4cab6a4b8219800608f767a6afd872855b
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-08 00:51:26 +0100

    Update ExternalTextureHelper.java

--- src/main/java/net/fexcraft/lib/mc/render/ExternalTextureHelper.java
+++ src/main/java/net/fexcraft/lib/mc/render/ExternalTextureHelper.java
@@ -12,15 +12,19 @@ import net.minecraft.client.renderer.texture.ITextureObject;
 public class ExternalTextureHelper {
 	
 	private static final Map<String, UCResourceLocation> map = new HashMap<String, UCResourceLocation>();
+	public static final String PREFIX = "fcl:remote/";
 	
 	public static UCResourceLocation get(String s){
-		if(map.containsKey(s)){ return map.get(s); }
-		UCResourceLocation texture = new UCResourceLocation("fcl:remote/", s);
+		if(map.containsKey(s)) return map.get(s);
+		UCResourceLocation texture = new UCResourceLocation(PREFIX, s);
 		ITextureObject object = Minecraft.getMinecraft().renderEngine.getTexture(texture);
-        if(object == null){ File file = new File(s);
+        if(object == null){
+        	File file = new File("./cache/fcl/img/" + s);
     		ThreadDownloadImageData tdid = new ThreadDownloadImageData(file.exists() ? file : null, s, null, null);
         	Minecraft.getMinecraft().renderEngine.loadTexture(texture, (object = tdid));
-        } map.put(s, texture); return texture;
+        }
+        map.put(s, texture);
+        return texture;
 	}
 	
 }
