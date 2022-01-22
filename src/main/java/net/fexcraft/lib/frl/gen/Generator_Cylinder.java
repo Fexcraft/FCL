22fb816cc1c022315481b5b8f0ee5828d20f7fd7
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-23 00:54:08 +0100

    Create Generator_Cylinder.java

---
+++ src/main/java/net/fexcraft/lib/frl/gen/Generator_Cylinder.java
@@ -0,0 +1,16 @@
+package net.fexcraft.lib.frl.gen;
+
+import net.fexcraft.lib.frl.Polyhedron;
+
+/**
+ * 
+ * @author Ferdinand Calo' (FEX___96)
+ *
+ */
+public class Generator_Cylinder {
+
+	public static <GLO> void make(Polyhedron<GLO> poly, ValueMap map){
+		//
+	}
+	
+}
