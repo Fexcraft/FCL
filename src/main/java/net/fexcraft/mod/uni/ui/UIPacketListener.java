a578d7a2625d41d0d0c754f0b2018587d48f7f99
Ferdinand Calo <fexcalo@gmail.com>
2024-06-30 23:26:49 +0200

    Create UIPacketListener.java

---
+++ src/main/java/net/fexcraft/mod/uni/ui/UIPacketListener.java
@@ -0,0 +1,53 @@
+package net.fexcraft.mod.uni.ui;
+
+import net.fexcraft.lib.mc.api.packet.IPacketListener;
+import net.fexcraft.lib.mc.api.packet.IPacketReceiver;
+import net.fexcraft.lib.mc.network.handlers.NBTTagCompoundPacketHandler;
+import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
+import net.fexcraft.mod.uni.EnvInfo;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.fml.relauncher.Side;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class UIPacketListener {
+
+	public static class Client implements IPacketListener<PacketNBTTagCompound> {
+
+		@Override
+		public String getId(){
+			return "fcl:ui";
+		}
+
+		@Override
+		public void process(PacketNBTTagCompound packet, Object[] objs){
+			((UniCon)((EntityPlayer)objs[0]).openContainer).container().packet(TagCW.wrap(packet.nbt), true);
+		}
+
+	}
+
+	public static class Server implements IPacketListener<PacketNBTTagCompound> {
+
+		@Override
+		public String getId(){
+			return "fcl:ui";
+		}
+
+		@Override
+		public void process(PacketNBTTagCompound packet, Object[] objs){
+			((UniCon)((EntityPlayer)objs[0]).openContainer).container().packet(TagCW.wrap(packet.nbt), false);
+		}
+
+	}
+
+
+	public static void register(){
+		if(EnvInfo.CLIENT){
+			NBTTagCompoundPacketHandler.addListener(Side.CLIENT, new UIPacketListener.Client());
+		}
+		NBTTagCompoundPacketHandler.addListener(Side.SERVER, new UIPacketListener.Server());
+	}
+
+}
