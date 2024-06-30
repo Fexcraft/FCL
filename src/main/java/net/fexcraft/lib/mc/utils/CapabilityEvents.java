870138789a55e6babbdfe13cfd0710df89f8736d
Ferdinand Calo <fexcalo@gmail.com>
2024-06-30 17:31:09 +0200

    Update CapabilityEvents.java

--- src/main/java/net/fexcraft/lib/mc/utils/CapabilityEvents.java
+++ src/main/java/net/fexcraft/lib/mc/utils/CapabilityEvents.java
@@ -1,21 +1,18 @@
 package net.fexcraft.lib.mc.utils;
 
-import net.fexcraft.lib.mc.capabilities.FCLCapabilities;
 import net.fexcraft.mod.uni.util.UniPlayerSerializer;
-import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod.EventBusSubscriber
 public class CapabilityEvents {
 
 	public static final ResourceLocation REGNAME = new ResourceLocation("fcl:player");
 
 	@SubscribeEvent
-	public static void onAttachEntityCapabilities(AttachCapabilitiesEvent<net.minecraft.entity.Entity> event){
-		if(FCLCapabilities.PLAYER != null && event.getObject() instanceof EntityLiving){
+	public void onAttachEntityCapabilities(AttachCapabilitiesEvent<net.minecraft.entity.Entity> event){
+		if(event.getObject() instanceof EntityLivingBase){
 			event.addCapability(REGNAME, new UniPlayerSerializer(event.getObject()));
 		}
 	}
