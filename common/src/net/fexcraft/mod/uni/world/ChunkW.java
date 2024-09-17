f71597fe9b9d0f8c64c0494e3b7b0331c221139c
Ferdinand Calo <fexcalo@gmail.com>
2024-09-17 11:01:42 +0200

    Update ChunkW.java

--- common/src/net/fexcraft/mod/uni/world/ChunkW.java
+++ common/src/net/fexcraft/mod/uni/world/ChunkW.java
@@ -14,9 +14,15 @@ public interface ChunkW {
 
 	public boolean isOnClient();
 
+	@Deprecated
 	public int getX();
 
+	@Deprecated
 	public int getZ();
+
+	public int x();
+
+	public int z();
 
 	public WorldW getWorld();
 
