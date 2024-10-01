19f020daaa3c44723ca329bdf21f56b4e06c9ae1
Ferdinand Calo <fexcalo@gmail.com>
2024-10-01 19:33:05 +0200

    Create SWProvider.java

---
+++ fabric20/src/main/java/net/fexcraft/mod/fcl/mixint/SWProvider.java
@@ -0,0 +1,12 @@
+package net.fexcraft.mod.fcl.mixint;
+
+import net.fexcraft.mod.uni.item.StackWrapper;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public interface SWProvider {
+
+	StackWrapper fcl_wrapper();
+
+}
