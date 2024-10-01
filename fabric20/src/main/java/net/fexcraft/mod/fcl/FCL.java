e71dd8a8c5594065750c025a188ef185fa8bb40c
Ferdinand Calo <fexcalo@gmail.com>
2024-10-01 19:32:26 +0200

    Update FCL.java

--- fabric20/src/main/java/net/fexcraft/mod/fcl/FCL.java
+++ fabric20/src/main/java/net/fexcraft/mod/fcl/FCL.java
@@ -2,25 +2,48 @@ package net.fexcraft.mod.fcl;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.fexcraft.mod.fcl.mixint.SWProvider;
+import net.fexcraft.mod.uni.item.ItemWrapper;
+import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.ui.UniCon;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
+import static net.minecraft.world.flag.FeatureFlags.*;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
 public class FCL implements ModInitializer {
 
 	public static final String MOD_ID = "fcl";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static Optional<MenuType<UniCon>> UNIVERSAL;
 
 	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+	public void onInitialize(){
+		FCL20.MAINDIR = FabricLoader.getInstance().getGameDirectory();
+		FCL20.init(FabricLoader.getInstance().isDevelopmentEnvironment(), false);//TODO
+		StackWrapper.SUPPLIER = obj -> {
+			if(obj instanceof ItemWrapper){
+				return StackWrapper.SUPPLIER.apply(new ItemStack((Item)((ItemWrapper)obj).local()));
+			}
+			if(obj instanceof ItemStack){
+				return ((SWProvider)obj).fcl_wrapper();
+			};
+			return StackWrapper.EMPTY;
+		};
+		//
+		MenuType<?> mt = Registry.register(BuiltInRegistries.MENU, "fcl:universal", (MenuType<?>)new MenuType((i, inv) -> new UniCon(i, inv, null), DEFAULT_FLAGS));
+		UNIVERSAL = Optional.of((MenuType<UniCon>)mt);
 	}
 
 }
