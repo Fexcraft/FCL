454a5069bda7179b5072a7b1032be9a28726a072
Ferdinand Calo <fexcalo@gmail.com>
2024-10-31 16:49:51 +0100

    Update GLO.java

--- common/src/net/fexcraft/lib/frl/GLO.java
+++ common/src/net/fexcraft/lib/frl/GLO.java
@@ -5,7 +5,10 @@ import java.util.function.Supplier;
 public class GLO<SELF extends GLO> {
 
 	public static Supplier<GLO> SUPPLIER = null;
+	public Material material = Material.NONE;
 
-	public void copy(SELF from, boolean full){}
+	public void copy(SELF from, boolean full){
+		material = from.material;
+	}
 
 }
