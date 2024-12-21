8b9a69938a11db364df17fc5c8155cd7c0d278f9
Ferdinand Calo <fexcalo@gmail.com>
2024-12-21 20:27:24 +0100

    Update UIUtils.java

--- forge12/src/main/java/net/fexcraft/mod/uni/ui/UIUtils.java
+++ forge12/src/main/java/net/fexcraft/mod/uni/ui/UIUtils.java
@@ -4,33 +4,21 @@ import net.fexcraft.app.json.JsonHandler;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.UniReg;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ResourceLocation;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class UIUtils {
 
-	public static JsonMap getMapC(String mod, UIKey key){
-		ResourceLocation loc = null;
-		try{
-			loc = new ResourceLocation(mod, UniReg.MENU_JSON_C.get(key) + ".json");
-			return JsonHandler.parse(net.minecraft.client.Minecraft.getMinecraft().getResourceManager().getResource(loc).getInputStream());
-		}
-		catch(Exception e){
-			Print.log("ui-loc: " + loc);
-			e.printStackTrace();
-			return new JsonMap();
-		}
-	}
-
-	public static JsonMap getMapS(String mod, UIKey key){
+	public static JsonMap getMap(String mod, UIKey key){
 		String loc = null;
 		try{
-			loc = UniReg.MENU_JSON_S.get(key) + ".json";
+			IDL idl = UniReg.MENU_JSON.get(key);
+			loc = "data/" + idl.space() + "/" + idl.path();
 			return JsonHandler.parse(UniReg.getInst(mod).getClass().getClassLoader().getResourceAsStream(loc));
 		}
 		catch(Exception e){
@@ -42,7 +30,7 @@ public class UIUtils {
 
 	public static Object getClient(String mod, int id, EntityPlayer player, int x, int y, int z){
 		UIKey key = UIKey.find(mod, id);
-		JsonMap map = getMapC(mod, key);
+		JsonMap map = getMap(mod, key);
 		try{
 			ContainerInterface con = UniReg.MENU.get(key).getConstructor(JsonMap.class, UniEntity.class, V3I.class).newInstance(map, UniEntity.get(player), new V3I(x, y, z));
 			return new UniUI(UniReg.GUI.get(key).getConstructor(JsonMap.class, ContainerInterface.class).newInstance(map, con), player);
@@ -55,7 +43,7 @@ public class UIUtils {
 
 	public static Object getServer(String mod, int id, EntityPlayer player, int x, int y, int z){
 		UIKey key = UIKey.find(mod, id);
-		JsonMap map = getMapS(mod, key);
+		JsonMap map = getMap(mod, key);
 		try{
 			return new UniCon(UniReg.MENU.get(key).getConstructor(JsonMap.class, UniEntity.class, V3I.class).newInstance(map, UniEntity.get(player), new V3I(x, y, z)), player);
 		}
