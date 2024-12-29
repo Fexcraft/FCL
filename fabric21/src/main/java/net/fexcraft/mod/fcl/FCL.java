a484a434c489133936fc9e8ebb37ff8808114d2b
Ferdinand Calo <fexcalo@gmail.com>
2024-12-29 21:42:46 +0100

    Update FCL.java

--- fabric21/src/main/java/net/fexcraft/mod/fcl/FCL.java
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/FCL.java
@@ -2,13 +2,17 @@ package net.fexcraft.mod.fcl;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerFactory;
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fexcraft.lib.common.math.AxisRotator;
-import net.fexcraft.lib.common.utils.Formatter;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fcl.mixint.EWProvider;
 import net.fexcraft.mod.fcl.mixint.SWProvider;
-import net.fexcraft.mod.fcl.util.Axis3DL;
-import net.fexcraft.mod.fcl.util.ChunkWI;
-import net.fexcraft.mod.fcl.util.EntityUtil;
+import net.fexcraft.mod.fcl.util.*;
 import net.fexcraft.mod.uni.EnvInfo;
 import net.fexcraft.mod.uni.UniChunk;
 import net.fexcraft.mod.uni.UniEntity;
@@ -20,18 +24,26 @@ import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.tag.TagLW;
 import net.fexcraft.mod.uni.ui.*;
 import net.fexcraft.mod.uni.world.StateWrapper;
-import net.minecraft.client.resources.language.I18n;
+import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.BlockItem;
@@ -46,6 +58,7 @@ import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,7 +66,7 @@ import java.io.File;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-import static net.minecraft.world.flag.FeatureFlags.DEFAULT_FLAGS;
+import static net.minecraft.commands.Commands.literal;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -65,12 +78,21 @@ public class FCL implements ModInitializer {
 	public static File GAMEDIR;
 	public static Supplier<MinecraftServer> SERVER = null;
 	//
-	public static Optional<MenuType<UniCon>> UNIVERSAL;
+	public static final ResourceLocation UI_PACKET = ResourceLocation.parse("fcl:ui");
+	public static final CustomPacketPayload.Type<UIPacket> UI_PACKET_TYPE = new CustomPacketPayload.Type<>(UI_PACKET);
+	public static final StreamCodec<RegistryFriendlyByteBuf, UIPacket> UI_PACKET_CODEC = StreamCodec.of(UIPacket::encode, UIPacket::new);
+	//
+	public static final ResourceLocation UI_SYNC = ResourceLocation.parse("fcl:ui_sync");
+	public static final CustomPacketPayload.Type<UISync> UI_SYNC_TYPE = new CustomPacketPayload.Type<>(UI_SYNC);
+	public static final StreamCodec<RegistryFriendlyByteBuf, UISync> UI_SYNC_CODEC = StreamCodec.of(UISync::encode, UISync::new);
+	//
+	public static ExtendedScreenHandlerType<UniCon, UISync> UNIVERSAL;
 
 	@Override
 	public void onInitialize(){
 		GAMEDIR = FabricLoader.getInstance().getGameDirectory();
 		init(FabricLoader.getInstance().isDevelopmentEnvironment(), false);
+		WrapperHolder.INSTANCE = new WrapperHolderImpl();
 		StackWrapper.SUPPLIER = obj -> {
 			if(obj instanceof ItemWrapper){
 				return StackWrapper.SUPPLIER.apply(new ItemStack((Item)((ItemWrapper)obj).local()));
@@ -80,10 +102,53 @@ public class FCL implements ModInitializer {
 			};
 			return StackWrapper.EMPTY;
 		};
+		UniEntity.GETTER = ent -> ((EWProvider)ent).fcl_wrapper();
 		//
-		MenuType<?> mt = Registry.register(BuiltInRegistries.MENU, "fcl:universal", (MenuType<?>)new MenuType((i, inv) -> new UniCon(i, inv, null), DEFAULT_FLAGS));
-		UNIVERSAL = Optional.of((MenuType<UniCon>)mt);
+		CommandRegistrationCallback.EVENT.register((dispatcher, registry, env) ->
+			dispatcher.register(literal("fcl").executes(com -> {
+				if(com.getSource().isPlayer()) UniEntity.getEntity(com.getSource().getPlayer()).openUI(UniFCL.SELECT_CONFIG, V3I.NULL);
+				return 1;
+			}))
+		);
 		//
+		PayloadTypeRegistry.playS2C().register(UI_PACKET_TYPE, UI_PACKET_CODEC);
+		PayloadTypeRegistry.playC2S().register(UI_PACKET_TYPE, UI_PACKET_CODEC);
+		ServerPlayNetworking.registerGlobalReceiver(UI_PACKET_TYPE, (packet, context) -> {
+			context.server().execute(() -> {
+				((UniCon)context.player().containerMenu).onPacket(packet.com(), false);
+			});
+		});
+		UNIVERSAL = Registry.register(BuiltInRegistries.MENU, "fcl:universal", new ExtendedScreenHandlerType<UniCon, UISync>(new ExtendedScreenHandlerType.ExtendedFactory<UniCon, UISync>() {
+			@Override
+			public UniCon create(int i, Inventory inventory, UISync sync){
+				return new UniCon(i, inventory, sync.key(), sync.pos(), sync.map());
+			}
+		}, UI_SYNC_CODEC));
+		//
+		EntityUtil.UI_OPENER = (player, ui, pos) -> {
+			try{
+				UIKey key = UIKey.find(ui);
+				player.openMenu(new ExtendedScreenHandlerFactory<>() {
+					@Override
+					public @Nullable AbstractContainerMenu createMenu(int i, Inventory inventory, Player player){
+						return new UniCon(0, inventory, key, pos, UniReg.getMenuJson(ui));
+					}
+
+					@Override
+					public Component getDisplayName(){
+						return Component.literal("Fexcraft Universal UI");
+					}
+
+					@Override
+					public Object getScreenOpeningData(ServerPlayer serverPlayer){
+						return new UISync(key, pos, UniReg.getMenuJson(ui));
+					}
+				});
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		};
 		UniFCL.registerUI(this);
 	}
 
@@ -139,14 +204,6 @@ public class FCL implements ModInitializer {
 		UniChunk.CHUNK_GETTER = ck -> new ChunkWI((LevelChunk)ck);
 		WrapperHolderImpl.LEVEL_PROVIDER = lvl -> new LevelW((Level)lvl);
 		UISlot.GETTERS.put("default", args -> new Slot((Container)args[0], (Integer)args[1], (Integer)args[2], (Integer)args[3]));
-		if(EnvInfo.CLIENT){
-			UITab.IMPLEMENTATION = UUITab.class;
-			UIText.IMPLEMENTATION = UUIText.class;
-			UIField.IMPLEMENTATION = UUIField.class;
-			UIButton.IMPLEMENTATION = UUIButton.class;
-			ContainerInterface.TRANSLATOR = str -> Formatter.format(I18n.get(str));
-			ContainerInterface.TRANSFORMAT = (str, objs) -> Formatter.format(I18n.get(str, objs));
-		}
 	}
 
 	private static Property<?> getProperty(BlockState state, String str){
