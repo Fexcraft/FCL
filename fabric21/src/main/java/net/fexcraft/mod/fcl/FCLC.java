a52ef8ad13c5eae9df59f9c8c241f4a9a9a92e63
Ferdinand Calo <fexcalo@gmail.com>
2024-12-29 21:42:49 +0100

    Create FCLC.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/FCLC.java
@@ -0,0 +1,36 @@
+package net.fexcraft.mod.fcl;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fexcraft.lib.common.utils.Formatter;
+import net.fexcraft.mod.uni.EnvInfo;
+import net.fexcraft.mod.uni.ui.*;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.resources.language.I18n;
+
+import static net.fexcraft.mod.fcl.FCL.*;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class FCLC implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient(){
+		EnvInfo.CLIENT = true;
+		MenuScreens.register(FCL.UNIVERSAL, UniUI::new);
+		ClientPlayNetworking.registerGlobalReceiver(UI_PACKET_TYPE, (packet, context) -> {
+			context.client().execute(() -> {
+				((UniCon)context.player().containerMenu).onPacket(packet.com(), true);
+			});
+		});
+		//
+		UITab.IMPLEMENTATION = UUITab.class;
+		UIText.IMPLEMENTATION = UUIText.class;
+		UIField.IMPLEMENTATION = UUIField.class;
+		UIButton.IMPLEMENTATION = UUIButton.class;
+		ContainerInterface.TRANSLATOR = str -> Formatter.format(I18n.get(str));
+		ContainerInterface.TRANSFORMAT = (str, objs) -> Formatter.format(I18n.get(str, objs));
+	}
+
+}
