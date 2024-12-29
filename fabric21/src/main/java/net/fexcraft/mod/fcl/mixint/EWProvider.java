19fc94d8d65fa86e0a1c4d1dd6607573adbe54d6
Ferdinand Calo <fexcalo@gmail.com>
2024-12-29 21:42:53 +0100

    Create EWProvider.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/mixint/EWProvider.java
@@ -0,0 +1,13 @@
+package net.fexcraft.mod.fcl.mixint;
+
+import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.world.EntityW;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public interface EWProvider {
+
+	UniEntity fcl_wrapper();
+
+}
