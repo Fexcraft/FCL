4e0ac4f526d16666347cb1e0c8314eaef9cb3a55
Ferdinand Calo <fexcalo@gmail.com>
2024-12-30 22:32:29 +0100

    Create CWProvider.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/mixint/CWProvider.java
@@ -0,0 +1,12 @@
+package net.fexcraft.mod.fcl.mixint;
+
+import net.fexcraft.mod.uni.UniChunk;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public interface CWProvider {
+
+	UniChunk fcl_wrapper();
+
+}
