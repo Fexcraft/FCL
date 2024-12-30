842238550fbeda5219cf0c6597d5d48414ca7fc1
Ferdinand Calo <fexcalo@gmail.com>
2024-12-30 22:32:27 +0100

    Create ChunkMixin.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/mixin/ChunkMixin.java
@@ -0,0 +1,28 @@
+package net.fexcraft.mod.fcl.mixin;
+
+import net.fexcraft.mod.fcl.mixint.CWProvider;
+import net.fexcraft.mod.fcl.mixint.EWProvider;
+import net.fexcraft.mod.uni.UniChunk;
+import net.fexcraft.mod.uni.UniEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.chunk.LevelChunk;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+@Mixin(LevelChunk.class)
+public class ChunkMixin implements CWProvider {
+
+	@Unique
+	public UniChunk wrapper;
+
+	@Override
+	public UniChunk fcl_wrapper(){
+		if(wrapper == null) wrapper = new UniChunk().set((LevelChunk)(Object)this);
+		return wrapper;
+	}
+
+}
