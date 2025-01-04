8241a4a11e1103dd519ebacc52d21355c7008f65
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-04 21:09:26 +0100

    Update GuiHandler.java

--- forge12/src/main/java/net/fexcraft/lib/mc/gui/GuiHandler.java
+++ forge12/src/main/java/net/fexcraft/lib/mc/gui/GuiHandler.java
@@ -1,9 +1,5 @@
 package net.fexcraft.lib.mc.gui;
 
-import net.fexcraft.lib.mc.crafting.BluePrintTableContainer2;
-import net.fexcraft.lib.mc.crafting.BluePrintTableGui0;
-import net.fexcraft.lib.mc.crafting.BluePrintTableGui1;
-import net.fexcraft.lib.mc.crafting.BluePrintTableGui2;
 import net.fexcraft.mod.uni.ui.UIUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
@@ -13,16 +9,11 @@ public class GuiHandler implements IGuiHandler {
 	
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
-		if(ID == 0 || ID == 1) return new GenericContainer.DefImpl(player);
-		if(ID == 2) return new BluePrintTableContainer2(player, world, x, y, z);
 		return UIUtils.getServer("fcl", ID, player, x, y, z);
 	}
 	
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
-		if(ID == 0) return new BluePrintTableGui0(player, world, x, y, z);
-		if(ID == 1) return new BluePrintTableGui1(player, world, x, y, z);
-		if(ID == 2) return new BluePrintTableGui2(player, world, x, y, z);
 		return UIUtils.getClient("fcl", ID, player, x, y, z);
 	}
 	
