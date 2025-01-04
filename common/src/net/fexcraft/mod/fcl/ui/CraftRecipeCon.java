1840a04a50c9b727a5861ebe34b52733677f4c38
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-04 16:06:56 +0100

    Update CraftRecipeCon.java

--- common/src/net/fexcraft/mod/fcl/ui/CraftRecipeCon.java
+++ common/src/net/fexcraft/mod/fcl/ui/CraftRecipeCon.java
@@ -5,11 +5,14 @@ import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fcl.UniFCL;
 import net.fexcraft.mod.uni.ConfigBase;
 import net.fexcraft.mod.uni.ConfigBase.ConfigEntry;
+import net.fexcraft.mod.uni.FclRecipe;
+import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -30,7 +33,15 @@ public class CraftRecipeCon extends ContainerInterface {
 		if(com.has("main")){
 			player.entity.openUI(UniFCL.SELECT_RECIPE_CATEGORY, V3I.NULL);
 		}
-		//
+		if(com.has("cat")){
+			int catidx = FclRecipe.indexOfCategory(com.getString("cat"));
+			if(com.has("res")){
+				player.entity.openUI(UniFCL.RECIPE_CRAFTING, catidx, FclRecipe.getResultIdx(com.getString("cat"), com.getString("res")), 0);
+			}
+			else{
+				player.entity.openUI(UniFCL.SELECT_RECIPE_RESULT, catidx, 0, 0);
+			}
+		}
 	}
 
 }
