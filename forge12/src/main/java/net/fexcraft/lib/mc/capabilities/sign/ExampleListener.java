e48c8ec0afe8457d4473cd6464e230d06695c519
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-05 18:02:43 +0100

    Update ExampleListener.java

--- forge12/src/main/java/net/fexcraft/lib/mc/capabilities/sign/ExampleListener.java
+++ forge12/src/main/java/net/fexcraft/lib/mc/capabilities/sign/ExampleListener.java
@@ -4,7 +4,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.fexcraft.lib.common.math.Time;
 import net.fexcraft.lib.common.utils.Formatter;
-import net.fexcraft.lib.mc.FCL;
+import net.fexcraft.mod.fcl.FCL;
 import net.fexcraft.lib.mc.network.Network;
 import net.fexcraft.lib.mc.utils.Print;
 import net.minecraft.block.state.IBlockState;
