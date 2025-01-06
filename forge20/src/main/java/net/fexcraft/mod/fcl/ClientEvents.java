ae8d95b9f2249f8c677393336c3b5bfecc529f48
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-06 21:01:56 +0100

    Update ClientEvents.java

--- forge20/src/main/java/net/fexcraft/mod/fcl/ClientEvents.java
+++ forge20/src/main/java/net/fexcraft/mod/fcl/ClientEvents.java
@@ -1,8 +1,12 @@
 package net.fexcraft.mod.fcl;
 
+import net.fexcraft.mod.fcl.local.CraftingRenderer;
 import net.fexcraft.mod.uni.ui.UniUI;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -15,6 +19,18 @@ public class ClientEvents {
     @SubscribeEvent
     public static void clientInit(FMLClientSetupEvent event){
         MenuScreens.register(FCL.UNIVERSAL.get(), UniUI::new);
+    }
+
+    @SubscribeEvent
+    public static void register(EntityRenderersEvent.RegisterRenderers event){
+        event.registerBlockEntityRenderer(FCL.CRAFTING_ENTITY.get(), con -> new CraftingRenderer());
+    }
+
+    @SubscribeEvent
+    public static void register(BuildCreativeModeTabContentsEvent event){
+        if(event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES || event.getTabKey() == CreativeModeTabs.INVENTORY){
+            event.accept(FCL.CRAFTING_ITEM.get());
+        }
     }
 
 }
