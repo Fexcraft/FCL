db246b126d470044888eccf07f1000a08d54edc1
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-06 22:45:21 +0100

    Update CraftingRenderer.java

--- forge20/src/main/java/net/fexcraft/mod/fcl/local/CraftingRenderer.java
+++ forge20/src/main/java/net/fexcraft/mod/fcl/local/CraftingRenderer.java
@@ -1,14 +1,21 @@
 package net.fexcraft.mod.fcl.local;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.fexcraft.lib.common.Static;
 import net.fexcraft.lib.tmt.ModelRendererTurbo;
 import net.fexcraft.mod.fcl.util.CraftingModel;
+import net.fexcraft.mod.fcl.util.FCLRenderTypes;
+import net.fexcraft.mod.fcl.util.Renderer120;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import org.joml.Quaternionf;
 
 import java.util.ArrayList;
+
+import static net.fexcraft.mod.fcl.local.CraftingBlock.FACING;
+import static net.fexcraft.mod.fcl.util.Renderer120.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -20,11 +27,16 @@ public class CraftingRenderer implements BlockEntityRenderer<CraftingEntity> {
 
 	@Override
 	public void render(CraftingEntity tile, float ticks, PoseStack pose, MultiBufferSource buffer, int light, int overlay){
+		Renderer120.pose = pose;
+		Renderer120.set(pose, buffer, light, overlay);
+		FCLRenderTypes.setCutout(TEXTURE);
 		pose.pushPose();
 		pose.translate(0.5, 0, 0.5);
+		pose.mulPose(new Quaternionf().rotateAxis(Static.toRadians(tile.getBlockState().getValue(FACING).toYRot()), AY));
+		pose.mulPose(new Quaternionf().rotateAxis(Static.rad180, AZ));
 		for(ArrayList<ModelRendererTurbo> group : MODEL.groups){
 			for(ModelRendererTurbo turbo : group){
-				//TODO turbo.render();
+				turbo.render();
 			}
 		}
 		pose.popPose();
