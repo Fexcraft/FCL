7581461f4ff9647190ee1e52bc0fb7c78f781594
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-12 16:01:35 +0100

    Update FCLC.java

--- fabric21/src/main/java/net/fexcraft/mod/fcl/FCLC.java
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/FCLC.java
@@ -4,11 +4,15 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fexcraft.lib.common.math.AxisRotator;
 import net.fexcraft.lib.common.utils.Formatter;
+import net.fexcraft.lib.tmt.ModelRendererTurbo;
+import net.fexcraft.mod.fcl.local.CraftingRenderer;
 import net.fexcraft.mod.fcl.util.Axis3DL;
+import net.fexcraft.mod.fcl.util.Renderer120;
 import net.fexcraft.mod.fcl.util.UIPacket;
 import net.fexcraft.mod.uni.EnvInfo;
 import net.fexcraft.mod.uni.ui.*;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.client.resources.language.I18n;
 
 import static net.fexcraft.mod.fcl.FCL.*;
@@ -22,12 +26,14 @@ public class FCLC implements ClientModInitializer {
 	public void onInitializeClient(){
 		EnvInfo.CLIENT = true;
 		MenuScreens.register(FCL.UNIVERSAL, UniUI::new);
+		BlockEntityRenderers.register(CRAFTING_ENTITY, context -> new CraftingRenderer());
 		ClientPlayNetworking.registerGlobalReceiver(UI_PACKET_TYPE, (packet, context) -> {
 			context.client().execute(() -> {
 				((UniCon)context.player().containerMenu).onPacket(packet.com().local(), true);
 			});
 		});
 		//
+		ModelRendererTurbo.RENDERER = new Renderer120();
 		AxisRotator.DefHolder.DEF_IMPL = Axis3DL.class;
 		UITab.IMPLEMENTATION = UUITab.class;
 		UIText.IMPLEMENTATION = UUIText.class;
