0725b8da73c819c9fb01d083f3104aa34431248e
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-18 10:45:41 +0100

    Create ExternalTextures.java

--- fabric21/src/main/java/net/fexcraft/mod/fcl/util/ExternalTextures.java
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/util/ExternalTextures.java
@@ -1,11 +1,15 @@
 package net.fexcraft.mod.fcl.util;
 
+import com.mojang.blaze3d.platform.NativeImage;
 import net.fexcraft.mod.uni.impl.ResLoc;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.SkinTextureDownloader;
+import net.minecraft.resources.ResourceLocation;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -14,8 +18,6 @@ import java.util.Map;
 public class ExternalTextures {
 
 	private static final Map<String, ResLoc> MAP = new HashMap<>();
-	private static final HashSet<String> KEY = new HashSet<>();
-	static{ KEY.add("documents"); }
 
 	public static ResLoc get(String mid, String url){
 		if(MAP.containsKey(url)) return MAP.get(url);
@@ -24,8 +26,22 @@ public class ExternalTextures {
 		File file = new File("./temp/fcl_download/" + texture.path());
 		if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
 		file.deleteOnExit();
-		//TODO Minecraft.getInstance().getTextureManager().register(texture, new HttpTexture(file, url, texture, false, null));
+		SkinTextureDownloader.downloadAndRegisterSkin(texture.local(), file.toPath(), url, false);
 		return texture;
+	}
+
+	public static ResourceLocation get(String s, byte[] arr) throws IOException {
+		if(MAP.containsKey(s)){
+			if(arr != null) MAP.remove(s);
+			else return MAP.get(s).local();
+		}
+		if(arr == null) return ResourceLocation.parse(s);
+		ResourceLocation tex = ResourceLocation.parse(s);
+		File file = new File("./temp/fcl_download/" + tex.getPath());
+		if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+		file.deleteOnExit();
+		Minecraft.getInstance().getTextureManager().register(tex, new DynamicTexture(NativeImage.read(arr)));
+		return tex;
 	}
 
 }
