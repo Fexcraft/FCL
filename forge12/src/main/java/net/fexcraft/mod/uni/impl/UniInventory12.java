7b7801b478e18e07ac2f8396e09dd8482323496b
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-21 21:52:01 +0100

    Update UniInventory12.java

--- forge12/src/main/java/net/fexcraft/mod/uni/impl/UniInventory12.java
+++ forge12/src/main/java/net/fexcraft/mod/uni/impl/UniInventory12.java
@@ -9,13 +9,19 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
+import net.minecraftforge.items.ItemStackHandler;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class UniInventory12 extends UniInventory implements IInventory {
+public class UniInventory12 extends UniInventory implements IInventory, IItemHandler {
+
+	private ItemStackHandler handler;
 
 	public UniInventory12(List<Object> local){
 		super(local);
@@ -23,6 +29,7 @@ public class UniInventory12 extends UniInventory implements IInventory {
 
 	public UniInventory12(int size){
 		super(NonNullList.withSize(size, ItemStack.EMPTY));
+		handler = new ItemStackHandler();
 	}
 
 	@Override
@@ -36,8 +43,59 @@ public class UniInventory12 extends UniInventory implements IInventory {
 	}
 
 	@Override
+	public int getSlots(){
+		return size();
+	}
+
+	@Override
 	public ItemStack getStackInSlot(int index){
 		return get(index).local();
+	}
+
+	@Override
+	public ItemStack insertItem(int idx, @Nonnull ItemStack stack, boolean sim){
+		if(stack.isEmpty()) return ItemStack.EMPTY;
+		int rem = stacksize;
+		StackWrapper local = get(idx);
+		if(!local.empty()){
+			if(!ItemHandlerHelper.canItemStacksStack(stack, local.local())) return stack;
+			rem -= local.count();
+		}
+		if(rem <= 0) return stack;
+		boolean over = stack.getCount() > rem;
+		if(!sim){
+			if(local.empty()){
+				set(idx, StackWrapper.wrap(over ? ItemHandlerHelper.copyStackWithSize(stack, rem) : stack));
+			}
+			else local.incr(over ? rem : stack.getCount());
+		}
+		return over ? ItemHandlerHelper.copyStackWithSize(stack,stack.getCount() - rem) : ItemStack.EMPTY;
+	}
+
+	@Override
+	public ItemStack extractItem(int idx, int am, boolean sim){
+		if(am == 0) return ItemStack.EMPTY;
+		StackWrapper stack = get(idx);
+		if(stack.empty()) return ItemStack.EMPTY;
+		int rem = am < stack.maxsize() ? am : stack.maxsize();
+		if(stack.count() < rem){
+			if(!sim) set(idx, StackWrapper.EMPTY);
+			return stack.local();
+		}
+		else{
+			if(!sim) set(idx, StackWrapper.wrap(ItemHandlerHelper.copyStackWithSize(stack.local(), stack.count() - rem)));
+			return ItemHandlerHelper.copyStackWithSize(stack.local(), stack.count() - rem);
+		}
+	}
+
+	@Override
+	public int getSlotLimit(int slot){
+		return stacksize;
+	}
+
+	@Override
+	public boolean isItemValid(int idx, ItemStack stack){
+		return valid(idx, StackWrapper.wrap(stack));
 	}
 
 	@Override
