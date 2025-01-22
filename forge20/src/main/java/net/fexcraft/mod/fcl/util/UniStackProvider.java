ab66abcab3da516086e3141330ff8c67789e82b5
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-22 14:11:46 +0100

    Create UniStackProvider.java

---
+++ forge20/src/main/java/net/fexcraft/mod/fcl/util/UniStackProvider.java
@@ -0,0 +1,49 @@
+package net.fexcraft.mod.fcl.util;
+
+import net.fexcraft.mod.uni.impl.SWI;
+import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.item.UniStack;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.common.capabilities.*;
+import net.minecraftforge.common.util.LazyOptional;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class UniStackProvider implements ICapabilitySerializable<CompoundTag> {
+
+	public static final Capability<UniStack> CAPABILITY = CapabilityManager.get(new CapabilityToken<>(){});
+	private LazyOptional<UniStack> optional;
+	private UniStack unistack;
+
+	public UniStackProvider(ItemStack stack){
+		unistack = new UniStack().set(stack);
+		optional = LazyOptional.of(() -> unistack);
+	}
+
+	@Override
+	public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> capability, @Nullable Direction direction){
+		return capability == CAPABILITY ? optional.cast() : LazyOptional.empty();
+	}
+
+	@Override
+	public CompoundTag serializeNBT(){
+		TagCW com = TagCW.create();
+		unistack.appended.save(com);
+		return com.local();
+	}
+
+	@Override
+	public void deserializeNBT(CompoundTag com){
+		if(com == null) return;
+		unistack.appended.load(TagCW.wrap(com));
+	}
+
+}
