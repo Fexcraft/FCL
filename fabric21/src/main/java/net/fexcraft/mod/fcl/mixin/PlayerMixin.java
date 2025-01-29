6a361b94ee5e0417201609586ff3833eee2a2963
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-29 19:56:29 +0100

    Update PlayerMixin.java

--- fabric21/src/main/java/net/fexcraft/mod/fcl/mixin/PlayerMixin.java
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/mixin/PlayerMixin.java
@@ -1,16 +1,8 @@
 package net.fexcraft.mod.fcl.mixin;
 
 import net.fexcraft.mod.fcl.mixint.EWProvider;
-import net.fexcraft.mod.fcl.mixint.SWProvider;
-import net.fexcraft.mod.fcl.util.EntityUtil;
-import net.fexcraft.mod.fcl.util.EntityWI;
 import net.fexcraft.mod.uni.UniEntity;
-import net.fexcraft.mod.uni.impl.SWI;
-import net.fexcraft.mod.uni.item.StackWrapper;
-import net.fexcraft.mod.uni.world.EntityW;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
@@ -25,7 +17,7 @@ public class PlayerMixin implements EWProvider {
 
 	@Override
 	public UniEntity fcl_wrapper(){
-		if(wrapper == null) wrapper = new UniEntity().set((Entity)(Object)this);
+		if(wrapper == null) wrapper = new UniEntity().set(this);
 		return wrapper;
 	}
 
