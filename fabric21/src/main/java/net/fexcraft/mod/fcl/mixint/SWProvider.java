58ff6a32fa48cc6437b57317f253b404618b267a
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-29 19:56:36 +0100

    Update SWProvider.java

--- fabric21/src/main/java/net/fexcraft/mod/fcl/mixint/SWProvider.java
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/mixint/SWProvider.java
@@ -1,12 +1,12 @@
 package net.fexcraft.mod.fcl.mixint;
 
-import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.inv.UniStack;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public interface SWProvider {
 
-	StackWrapper fcl_wrapper();
+	UniStack fcl_wrapper();
 
 }
