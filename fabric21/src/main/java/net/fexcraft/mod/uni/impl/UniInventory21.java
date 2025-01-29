731498c49f1d5cba32329844dc7418b441f242cb
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-29 19:56:50 +0100

    Create UniInventory21.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/uni/impl/UniInventory21.java
@@ -0,0 +1,70 @@
+package net.fexcraft.mod.uni.impl;
+
+import net.fexcraft.mod.uni.inv.UniInventory;
+import net.fexcraft.mod.uni.inv.UniStack;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class UniInventory21 extends UniInventory implements Container {
+
+	public UniInventory21(List<Object> local){
+		super(local);
+	}
+
+	public UniInventory21(int size){
+		super(NonNullList.withSize(size, ItemStack.EMPTY));
+	}
+
+	@Override
+	public int getContainerSize(){
+		return size();
+	}
+
+	@Override
+	public boolean isEmpty(){
+		return empty();
+	}
+
+	@Override
+	public ItemStack getItem(int idx){
+		return get(idx).local();
+	}
+
+	@Override
+	public ItemStack removeItem(int idx, int am){
+		return get(idx, am).local();
+	}
+
+	@Override
+	public ItemStack removeItemNoUpdate(int idx){
+		return remove(idx).local();
+	}
+
+	@Override
+	public void setItem(int idx, ItemStack stack){
+		set(idx, UniStack.getStack(stack));
+	}
+
+	@Override
+	public void setChanged(){
+		mark();
+	}
+
+	@Override
+	public boolean stillValid(Player player){
+		return true;
+	}
+
+	@Override
+	public void clearContent(){
+		clear();
+	}
+
+}
