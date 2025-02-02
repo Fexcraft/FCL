b8c617dd79ac963299a252ee02e31ed174292be2
Ferdinand Calo <fercalo96@yahoo.de>
2025-02-02 22:50:20 +0100

    Update UniFluidTank12.java

--- forge12/src/main/java/net/fexcraft/mod/uni/impl/UniFluidTank12.java
+++ forge12/src/main/java/net/fexcraft/mod/uni/impl/UniFluidTank12.java
@@ -13,6 +13,8 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
@@ -47,6 +49,22 @@ public class UniFluidTank12 extends UniFluidTank {
 	@Override
 	public void load(TagCW com){
 		tank.readFromNBT(com.local());
+	}
+
+	@Override
+	public <T> T local(){
+		return (T)tank;
+	}
+
+	@Override
+	public String getFluidFromStack(StackWrapper sw){
+		ItemStack stack = sw.local();
+		if(!stack.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null)) return null;
+		IFluidHandlerItem item = stack.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null);
+		if(item.getTankProperties().length > 0 && item.getTankProperties()[0].getContents() != null){
+			return item.getTankProperties()[0].getContents().getFluid().getBlock().getRegistryName().toString();
+		}
+		return null;
 	}
 
 }
