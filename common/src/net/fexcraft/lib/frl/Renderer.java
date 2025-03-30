1881fa29cf29484877325fd6805963f308a4f31c
Ferdinand Calo <fexcalo@gmail.com>
2025-03-30 01:56:23 +0100

    Update Renderer.java

--- common/src/net/fexcraft/lib/frl/Renderer.java
+++ common/src/net/fexcraft/lib/frl/Renderer.java
@@ -1,5 +1,7 @@
 package net.fexcraft.lib.frl;
 
+import net.fexcraft.lib.common.math.V3D;
+
 /**
  * 
  * @author Ferdinand Calo' (FEX___96)
@@ -11,9 +13,23 @@ public abstract class Renderer<GL extends GLO> {
 	public static Renderer RENDERER = new DefaultRenderer();
 	
 	public static boolean TRIANGULATED_QUADS = true;
-	
+
 	public abstract void render(Polyhedron<GL> poly);
 
 	public abstract void delete(Polyhedron<GL> poly);
+
+	public abstract void push();
+
+	public abstract void pop();
+
+	public void translate(V3D vec){
+		translate(vec.x, vec.y, vec.z);
+	}
+
+	public abstract void translate(double x, double y, double z);
+
+	public abstract void rotate(float deg, int x, int y, int z);
+
+	public abstract void scale(double x, double y, double z);
 
 }
