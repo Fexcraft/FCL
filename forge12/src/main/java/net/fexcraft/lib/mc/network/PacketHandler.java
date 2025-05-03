9b142bfb717913876301d14b28e7abc51c66a82c
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 14:58:47 +0200

    Update PacketHandler.java

--- forge12/src/main/java/net/fexcraft/lib/mc/network/PacketHandler.java
+++ forge12/src/main/java/net/fexcraft/lib/mc/network/PacketHandler.java
@@ -6,8 +6,6 @@ import net.fexcraft.mod.uni.impl.PacketFileHandler;
 import net.fexcraft.mod.uni.impl.PacketFileHandler.I12_PacketImg;
 import net.fexcraft.lib.mc.network.packet.Packet;
 import net.fexcraft.lib.mc.network.packet.PacketEntityUpdate;
-import net.fexcraft.lib.mc.network.packet.PacketJsonObject;
-import net.fexcraft.lib.mc.network.packet.PacketKeyInput;
 import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.lib.mc.network.packet.PacketTileEntityUpdate;
 import net.fexcraft.lib.mc.utils.Print;
@@ -17,23 +15,17 @@ import net.minecraftforge.fml.relauncher.Side;
 
 public class PacketHandler {
 	
-	private static final SimpleNetworkWrapper instance = NetworkRegistry.INSTANCE.newSimpleChannel("frsm");
-	public static int nextpacketid = 12;
+	private static final SimpleNetworkWrapper instance = NetworkRegistry.INSTANCE.newSimpleChannel("fcl");
 	
 	public static void init(){
 		Print.log("Initialising Packet Handler.");
 		instance.registerMessage(ExamplePacketHandler.class,                  Packet.class,                  0, Side.SERVER);
 		instance.registerMessage(TileEntityUpdatePacketHandler.Client.class,  PacketTileEntityUpdate.class,  1, Side.CLIENT);
 		instance.registerMessage(TileEntityUpdatePacketHandler.Server.class,  PacketTileEntityUpdate.class,  2, Side.SERVER);
-		instance.registerMessage(KeyInputPacketHandler.class,                 PacketKeyInput.class,          3, Side.SERVER);
-		//instance.registerMessage(ISUPacketHandler.Server.class,               PacketItemStackUpdate.class,   4, Side.SERVER);
-		//instance.registerMessage(ISUPacketHandler.Client.class,               PacketItemStackUpdate.class,   5, Side.CLIENT);
-		instance.registerMessage(JsonObjectPacketHandler.Server.class,        PacketJsonObject.class,        6, Side.SERVER);
-		instance.registerMessage(JsonObjectPacketHandler.Client.class,        PacketJsonObject.class,        7, Side.CLIENT);
-		instance.registerMessage(NBTTagCompoundPacketHandler.Server.class,    PacketNBTTagCompound.class,    8, Side.SERVER);
-		instance.registerMessage(NBTTagCompoundPacketHandler.Client.class,    PacketNBTTagCompound.class,    9, Side.CLIENT);
-		instance.registerMessage(EntityUpdatePacketHandler.Server.class,      PacketEntityUpdate.class,     10, Side.SERVER);
-		instance.registerMessage(EntityUpdatePacketHandler.Client.class,      PacketEntityUpdate.class,     11, Side.CLIENT);
+		instance.registerMessage(NBTTagCompoundPacketHandler.Server.class,    PacketNBTTagCompound.class,    3, Side.SERVER);
+		instance.registerMessage(NBTTagCompoundPacketHandler.Client.class,    PacketNBTTagCompound.class,    4, Side.CLIENT);
+		instance.registerMessage(EntityUpdatePacketHandler.Server.class,      PacketEntityUpdate.class,      5, Side.SERVER);
+		instance.registerMessage(EntityUpdatePacketHandler.Client.class,      PacketEntityUpdate.class,      6, Side.CLIENT);
 		instance.registerMessage(PacketFileHandler.Client.class, I12_PacketImg.class, 12, Side.CLIENT);
 		instance.registerMessage(PacketFileHandler.Server.class, I12_PacketImg.class, 13, Side.SERVER);
 		Print.log("Done initialising Packet Handler.");
@@ -44,46 +36,14 @@ public class PacketHandler {
 	}
 	
 	public static enum PacketHandlerType {
-		TILEENTITY, KEYINPUT, ITEMSTACK, JSON, NBT, ENTITY;
+		NBT;
 	}
 	
 	@SuppressWarnings("unchecked")
 	public static void registerListener(PacketHandlerType type, Side side, IPacketListener<?> listener){
 		switch(type){
-			/*case ITEMSTACK:{
-				ISUPacketHandler.addListener(side, (IPacketListener<PacketItemStackUpdate>)listener);
-				break;
-			}*/
-			case JSON:{
-				JsonObjectPacketHandler.addListener(side, (IPacketListener<PacketJsonObject>)listener);
-				break;
-			}
 			case NBT:{
 				NBTTagCompoundPacketHandler.addListener(side, (IPacketListener<PacketNBTTagCompound>) listener);
-				break;
-			}
-			case KEYINPUT:{
-				if(side.isServer()){
-					KeyInputPacketHandler.addListener((IPacketListener<PacketKeyInput>) listener);
-				}
-				break;
-			}
-			case TILEENTITY:{
-				if(side.isClient()){
-					TileEntityUpdatePacketHandler.Client.addListener((IPacketListener<PacketTileEntityUpdate>) listener);
-				}
-				if(side.isServer()){
-					TileEntityUpdatePacketHandler.Server.addListener((IPacketListener<PacketTileEntityUpdate>) listener);
-				}
-				break;
-			}
-			case ENTITY:{
-				if(side.isClient()){
-					EntityUpdatePacketHandler.Client.addListener((IPacketListener<PacketEntityUpdate>) listener);
-				}
-				if(side.isServer()){
-					EntityUpdatePacketHandler.Server.addListener((IPacketListener<PacketEntityUpdate>) listener);
-				}
 				break;
 			}
 			default: break;
