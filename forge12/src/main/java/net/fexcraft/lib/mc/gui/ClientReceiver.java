b25ddb1fc916fac9aff18ae6602e9d2d9c11102e
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 23:46:50 +0200

    Update ClientReceiver.java

--- forge12/src/main/java/net/fexcraft/lib/mc/gui/ClientReceiver.java
+++ forge12/src/main/java/net/fexcraft/lib/mc/gui/ClientReceiver.java
@@ -2,26 +2,24 @@ package net.fexcraft.lib.mc.gui;
 
 import net.fexcraft.lib.mc.api.packet.IPacketListener;
 import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
+import net.fexcraft.mod.uni.packet.PacketTagListener;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.relauncher.Side;
 
-public class ClientReceiver implements IPacketListener<PacketNBTTagCompound> {
+public class ClientReceiver implements PacketTagListener {
 
 	@Override
-	public String getId(){
-		return "fcl_gui";
-	}
-
-	@Override
-	public void process(PacketNBTTagCompound packet, Object[] objs){
-		if(!packet.nbt.hasKey("task")) return;
-        EntityPlayer player = (EntityPlayer)objs[0];
-		switch(packet.nbt.getString("task")){
-	        case "packet_gui":{
-	            ((GenericContainer)player.openContainer).packet(Side.CLIENT, packet.nbt, player);
-	        	return;
-	        }
+	public void handle(TagCW packet, EntityW entity){
+		if(!packet.has("task")) return;
+		EntityPlayer player = entity.local();
+		switch(packet.getString("task")){
+			case "packet_gui":{
+				((GenericContainer)player.openContainer).packet(Side.CLIENT, packet.local(), player);
+				return;
+			}
 		}
 	}
-	
+
 }
