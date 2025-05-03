43c04f2fac13d1d4f81878994d7a240a3600b6f2
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 23:46:54 +0200

    Update ServerReceiver.java

--- forge12/src/main/java/net/fexcraft/lib/mc/gui/ServerReceiver.java
+++ forge12/src/main/java/net/fexcraft/lib/mc/gui/ServerReceiver.java
@@ -3,37 +3,30 @@ package net.fexcraft.lib.mc.gui;
 import net.fexcraft.mod.fcl.FCL;
 import net.fexcraft.lib.mc.api.packet.IPacketListener;
 import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
+import net.fexcraft.mod.uni.packet.PacketTagListener;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.relauncher.Side;
 
-public class ServerReceiver implements IPacketListener<PacketNBTTagCompound> {
-	
-	public static ServerReceiver INSTANCE;
-	
-	public ServerReceiver(){ INSTANCE = this; }
+public class ServerReceiver implements PacketTagListener {
 
 	@Override
-	public String getId(){
-		return "fcl_gui";
-	}
-
-	@Override
-	public void process(PacketNBTTagCompound packet, Object[] objs){
-		if(!packet.nbt.hasKey("task")) return;
-		EntityPlayerMP player = (EntityPlayerMP)objs[0];
-		switch(packet.nbt.getString("task")){
+	public void handle(TagCW packet, EntityW entity){
+		if(!packet.has("task")) return;
+		EntityPlayerMP player = entity.local();
+		switch(packet.getString("task")){
 			case "open_gui":{
-                int gui = packet.nbt.getInteger("gui");
-                int[] args = packet.nbt.hasKey("args") ? packet.nbt.getIntArray("args") : new int[3];
-                player.openGui(FCL.getInstance(), gui, player.world, args[0], args[1], args[2]);
-                return;
+				int gui = packet.getInteger("gui");
+				int[] args = packet.has("args") ? packet.getIntArray("args") : new int[3];
+				player.openGui(FCL.getInstance(), gui, player.world, args[0], args[1], args[2]);
+				return;
 			}
-            case "packet_gui":{
-                ((GenericContainer)player.openContainer).packet(Side.SERVER, packet.nbt, player);
-            	return;
-            }
-            //
+			case "packet_gui":{
+				((GenericContainer)player.openContainer).packet(Side.SERVER, packet.local(), player);
+				return;
+			}
 		}
 	}
-	
+
 }
