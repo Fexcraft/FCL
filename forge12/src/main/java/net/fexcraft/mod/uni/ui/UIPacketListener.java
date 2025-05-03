dc299aabe6df91f3f592ff4df7f341f67f3a6a6b
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 23:47:06 +0200

    Update UIPacketListener.java

--- forge12/src/main/java/net/fexcraft/mod/uni/ui/UIPacketListener.java
+++ forge12/src/main/java/net/fexcraft/mod/uni/ui/UIPacketListener.java
@@ -1,64 +1,44 @@
 package net.fexcraft.mod.uni.ui;
 
-import net.fexcraft.lib.mc.api.packet.IPacketListener;
-import net.fexcraft.lib.mc.api.packet.IPacketReceiver;
-import net.fexcraft.lib.mc.network.handlers.NBTTagCompoundPacketHandler;
-import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.mod.fcl.FCL;
-import net.fexcraft.mod.uni.EnvInfo;
+import net.fexcraft.mod.uni.packet.PacketTagListener;
 import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.fml.relauncher.Side;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class UIPacketListener {
 
-	public static class Client implements IPacketListener<PacketNBTTagCompound> {
+	public static class Client implements PacketTagListener {
 
 		@Override
-		public String getId(){
-			return "fcl:ui";
-		}
-
-		@Override
-		public void process(PacketNBTTagCompound packet, Object[] objs){
-			if(((EntityPlayer)objs[0]).openContainer instanceof UniCon == false){
+		public void handle(TagCW packet, EntityW entity){
+			EntityPlayer player = entity.local();
+			if(player.openContainer instanceof UniCon == false){
 				FCL.LOGGER.info("ERROR: Received packet for UniCon, but Container is of different type.");
-				FCL.LOGGER.info("PACKET: " + packet.nbt);
+				FCL.LOGGER.info("PACKET: " + packet);
 				return;
 			}
-			((UniCon)((EntityPlayer)objs[0]).openContainer).container().packet(TagCW.wrap(packet.nbt), true);
+			((UniCon)player.openContainer).container().packet(packet, true);
 		}
 
 	}
 
-	public static class Server implements IPacketListener<PacketNBTTagCompound> {
+	public static class Server implements PacketTagListener {
 
 		@Override
-		public String getId(){
-			return "fcl:ui";
-		}
-
-		@Override
-		public void process(PacketNBTTagCompound packet, Object[] objs){
-			if(((EntityPlayer)objs[0]).openContainer instanceof UniCon == false){
+		public void handle(TagCW packet, EntityW entity){
+			EntityPlayer player = entity.local();
+			if(player.openContainer instanceof UniCon == false){
 				FCL.LOGGER.info("ERROR: Received packet for UniCon, but Container is of different type.");
-				FCL.LOGGER.info("PACKET: " + packet.nbt);
+				FCL.LOGGER.info("PACKET: " + packet);
 				return;
 			}
-			((UniCon)((EntityPlayer)objs[0]).openContainer).container().packet(TagCW.wrap(packet.nbt), false);
+			((UniCon)player.openContainer).container().packet(packet, false);
 		}
 
-	}
-
-
-	public static void register(){
-		if(EnvInfo.CLIENT){
-			NBTTagCompoundPacketHandler.addListener(Side.CLIENT, new UIPacketListener.Client());
-		}
-		NBTTagCompoundPacketHandler.addListener(Side.SERVER, new UIPacketListener.Server());
 	}
 
 }
