824cc8c8fad5074c4aada47ca504ca0c03b1b3a3
Ferdinand Calo <fexcalo@gmail.com>
2025-05-04 17:45:44 +0200

    Update FCL.java

--- fabric21/src/main/java/net/fexcraft/mod/fcl/FCL.java
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/FCL.java
@@ -41,7 +41,6 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
