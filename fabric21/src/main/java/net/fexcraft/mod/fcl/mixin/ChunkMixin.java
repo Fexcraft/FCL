511555096855cbc05adf4a890fc1a851bb85cb95
Ferdinand Calo <fexcalo@gmail.com>
2025-05-04 17:45:46 +0200

    Update ChunkMixin.java

--- fabric21/src/main/java/net/fexcraft/mod/fcl/mixin/ChunkMixin.java
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/mixin/ChunkMixin.java
@@ -1,11 +1,7 @@
 package net.fexcraft.mod.fcl.mixin;
 
 import net.fexcraft.mod.fcl.mixint.CWProvider;
-import net.fexcraft.mod.fcl.mixint.EWProvider;
 import net.fexcraft.mod.uni.UniChunk;
-import net.fexcraft.mod.uni.UniEntity;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.chunk.LevelChunk;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
