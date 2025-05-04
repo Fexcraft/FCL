878a8776299345384f881660d93123502b5f521b
Ferdinand Calo <fexcalo@gmail.com>
2025-05-04 18:03:58 +0200

    Update EntityMixin.java

--- fabric21/src/main/java/net/fexcraft/mod/fcl/mixin/EntityMixin.java
+++ fabric21/src/main/java/net/fexcraft/mod/fcl/mixin/EntityMixin.java
@@ -2,15 +2,15 @@ package net.fexcraft.mod.fcl.mixin;
 
 import net.fexcraft.mod.fcl.mixint.EWProvider;
 import net.fexcraft.mod.uni.UniEntity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-@Mixin(Player.class)
-public class PlayerMixin implements EWProvider {
+@Mixin(Entity.class)
+public class EntityMixin implements EWProvider {
 
 	@Unique
 	public UniEntity wrapper;
