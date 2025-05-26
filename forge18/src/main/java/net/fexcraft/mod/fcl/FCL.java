cd67eccf6330bb245750ba9dc0e49c6f0b54674a
Ferdinand Calo <fexcalo@gmail.com>
2025-05-26 22:20:11 +0200

    Update FCL.java

--- forge18/src/main/java/net/fexcraft/mod/fcl/FCL.java
+++ forge18/src/main/java/net/fexcraft/mod/fcl/FCL.java
@@ -4,10 +4,14 @@ import com.mojang.logging.LogUtils;
 import io.netty.buffer.ByteBuf;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.ui.UniCon;
+import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.extensions.IForgeMenuType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
@@ -18,6 +22,9 @@ import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 import java.io.File;
@@ -28,9 +35,21 @@ public class FCL {
 
     public static final Logger LOGGER = LogUtils.getLogger();
     public static UniFCL CONFIG;
+    public static String MODID = "fcl";
+    //
+    public static final DeferredRegister<MenuType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, MODID);
+    public static final RegistryObject<MenuType<UniCon>> UNIVERSAL = CONTAINERS.register("universal", () -> {
+        try{
+            return IForgeMenuType.create(UniCon::new);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    });
 
     public FCL(){
-        CONFIG = new UniFCL(new File(FMLPaths.CONFIGDIR.get().toFile(), "fcl.json"));
+        CONFIG = new UniFCL(FMLPaths.CONFIGDIR.get().toFile());
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         MinecraftForge.EVENT_BUS.register(this);
     }
