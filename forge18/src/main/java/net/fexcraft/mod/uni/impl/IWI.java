1229ab07feb48919c023ecae09c4f85a0865f4f2
Ferdinand Calo <fexcalo@gmail.com>
2025-07-15 21:30:14 +0200

    Create IWI.java

---
+++ forge18/src/main/java/net/fexcraft/mod/uni/impl/IWI.java
@@ -0,0 +1,29 @@
+package net.fexcraft.mod.uni.impl;
+
+import net.fexcraft.mod.uni.inv.ItemWrapper;
+import net.minecraft.world.item.Item;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class IWI extends ItemWrapper {
+
+	public Item item;
+
+	public IWI(Item item){
+		this.item = item;
+	}
+
+	public void linkContainer(){}
+
+	public void regToDict(){}
+
+	public Item local(){
+		return this.item;
+	}
+
+	public Object direct(){
+		return this.item;
+	}
+
+}
