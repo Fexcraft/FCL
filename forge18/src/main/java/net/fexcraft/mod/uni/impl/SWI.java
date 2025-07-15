8a077606637ebaaf7fc9322a7792ffdd1eb36cf6
Ferdinand Calo <fexcalo@gmail.com>
2025-07-15 21:30:16 +0200

    Create SWI.java

---
+++ forge18/src/main/java/net/fexcraft/mod/uni/impl/SWI.java
@@ -0,0 +1,137 @@
+package net.fexcraft.mod.uni.impl;
+
+import net.fexcraft.mod.uni.IDL;
+import net.fexcraft.mod.uni.IDLManager;
+import net.fexcraft.mod.uni.inv.ItemWrapper;
+import net.fexcraft.mod.uni.inv.StackWrapper;
+import net.fexcraft.mod.uni.inv.UniStack;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class SWI extends StackWrapper {
+
+	public ItemStack stack;
+
+	protected SWI(ItemWrapper item){
+		super(item);
+		stack = new ItemStack((ItemLike)item.direct());
+	}
+
+	@Override
+	public void set(Object obj){
+		stack = (ItemStack)obj;
+	}
+
+	protected SWI(ItemStack stack){
+		super(new IWI(stack.getItem()));
+		this.stack = stack;
+	}
+
+	public static SWI parse(Object obj){
+		if(obj instanceof ItemWrapper) return new SWI((ItemWrapper)obj);
+		if(obj instanceof ItemStack) return new SWI((ItemStack)obj);
+		if(obj instanceof TagCW) return new SWI(ItemStack.of((CompoundTag)((TagCW)obj).direct()));
+		return null;
+	}
+
+	public ItemStack local(){
+		return stack;
+	}
+
+	public Object direct(){
+		return stack;
+	}
+
+	@Override
+	public StackWrapper updateTag(TagCW tag){
+		stack.setTag(tag.local());
+		return this;
+	}
+
+	@Override
+	public TagCW directTag(){
+		if(!stack.hasTag()) stack.setTag(new CompoundTag());
+		return TagCW.wrap(stack.getTag());
+	}
+
+	@Override
+	public TagCW copyTag(){
+		if(!stack.hasTag()) stack.setTag(new CompoundTag());
+		return TagCW.wrap(stack.getTag().copy());
+	}
+
+	@Override
+	public boolean hasTag(){
+		return stack.hasTag();
+	}
+
+	@Override
+	public String getName(){
+		return stack.getDisplayName().getString();
+	}
+
+	@Override
+	public int maxsize(){
+		return stack.getMaxStackSize();
+	}
+
+	@Override
+	public int damage(){
+		return stack.getDamageValue();
+	}
+
+	@Override
+	public void damage(int val){
+		stack.setDamageValue(val);
+	}
+
+	@Override
+	public int count(){
+		return stack.getCount();
+	}
+
+	@Override
+	public void count(int am){
+		stack.setCount(am);
+	}
+
+	@Override
+	public StackWrapper copy(){
+		return UniStack.createStack(stack.copy());
+	}
+
+	@Override
+	public void save(TagCW com){
+		stack.save(com.local());
+	}
+
+	@Override
+	public boolean empty(){
+		return stack.isEmpty();
+	}
+
+	@Override
+	public IDL getIDL(){
+		return IDLManager.getIDL(getID());
+	}
+
+	@Override
+	public String getID(){
+		return ForgeRegistries.ITEMS.getKey(stack.getItem()).toString();
+	}
+
+	@Override
+	public boolean equals(Object o){
+		if(o instanceof StackWrapper){
+			return equals(((StackWrapper)o).direct());
+		}
+		else if(o instanceof ItemStack){
+			return ItemStack.isSameItemSameTags(stack, (ItemStack)o);
+		}
+		else return super.equals(o);
+	}
+
+}
