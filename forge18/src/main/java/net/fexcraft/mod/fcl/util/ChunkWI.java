9577d4637bb6cfd1ccebf81a6d59c2aaf3b21bd6
Ferdinand Calo <fexcalo@gmail.com>
2025-07-15 21:30:03 +0200

    Create ChunkWI.java

---
+++ forge18/src/main/java/net/fexcraft/mod/fcl/util/ChunkWI.java
@@ -0,0 +1,64 @@
+package net.fexcraft.mod.fcl.util;
+
+import net.fexcraft.mod.uni.world.ChunkW;
+import net.fexcraft.mod.uni.world.WorldW;
+import net.fexcraft.mod.uni.world.WrapperHolder;
+import net.minecraft.world.level.chunk.LevelChunk;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ChunkWI implements ChunkW {
+
+	private LevelChunk chunk;
+
+	public ChunkWI(LevelChunk ck){
+		chunk = ck;
+	}
+
+	@Override
+	public boolean isOnClient(){
+		return chunk.getLevel().isClientSide;
+	}
+
+	@Override
+	public int getX(){
+		return chunk.getPos().x;
+	}
+
+	@Override
+	public int getZ(){
+		return chunk.getPos().z;
+	}
+
+	@Override
+	public int x(){
+		return chunk.getPos().x;
+	}
+
+	@Override
+	public int z(){
+		return chunk.getPos().z;
+	}
+
+	@Override
+	public WorldW getWorld(){
+		return WrapperHolder.getWorld(chunk.getLevel());
+	}
+
+	@Override
+	public <E> E local(){
+		return (E)chunk;
+	}
+
+	@Override
+	public Object direct(){
+		return chunk;
+	}
+
+	@Override
+	public void markChanged(){
+		chunk.setUnsaved(true);
+	}
+
+}
