e761865b340eb9921c814403fc71b0cce1acf332
Ferdinand Calo <fexcalo@gmail.com>
2025-07-28 23:20:13 +0200

    Update SelRecipeCon.java

--- common/src/net/fexcraft/mod/fcl/ui/SelRecipeCon.java
+++ common/src/net/fexcraft/mod/fcl/ui/SelRecipeCon.java
@@ -6,12 +6,17 @@ import net.fexcraft.mod.fcl.UniFCL;
 import net.fexcraft.mod.uni.FclRecipe;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.tag.TagLW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
+
+import java.util.ArrayList;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class SelRecipeCon extends ContainerInterface {
+
+	protected ArrayList<String> categories = new ArrayList<>();
 
 	public SelRecipeCon(JsonMap map, UniEntity ply, V3I pos){
 		super(map, ply, pos);
@@ -24,6 +29,23 @@ public class SelRecipeCon extends ContainerInterface {
 
 	@Override
 	public void packet(TagCW com, boolean client){
+		if(com.has("sync_cat")){
+			if(client){
+				categories.clear();
+				TagLW lw = com.getList("cats");
+				int am = com.getInteger("cata");
+				for(int i = 0; i < am; i++){
+					categories.add(lw.getString(i));
+				}
+			}
+			else{
+				TagLW list = TagLW.create();
+				for(String str : FclRecipe.RECIPES.keySet()) list.add(str);
+				com.set("cats", list);
+				com.set("cata", list.size());
+				SEND_TO_CLIENT.accept(com, player);
+			}
+		}
 		if(com.has("main")){
 			player.entity.openUI(UniFCL.SELECT_RECIPE_CATEGORY, V3I.NULL);
 		}
