40acd3cc69c8287707501ab05aa614aab4a3934f
Ferdinand Calo <fexcalo@gmail.com>
2025-07-29 00:28:37 +0200

    Update SelRecipeCon.java

--- common/src/net/fexcraft/mod/fcl/ui/SelRecipeCon.java
+++ common/src/net/fexcraft/mod/fcl/ui/SelRecipeCon.java
@@ -4,19 +4,26 @@ import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fcl.UniFCL;
 import net.fexcraft.mod.uni.FclRecipe;
+import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.inv.StackWrapper;
+import net.fexcraft.mod.uni.inv.UniStack;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.tag.TagLW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class SelRecipeCon extends ContainerInterface {
 
-	protected ArrayList<String> categories = new ArrayList<>();
+	public static ArrayList<String> categories = new ArrayList<>();
+	public static ArrayList<StackWrapper> results = new ArrayList<>();
+	public static ArrayList<String> reskeys = new ArrayList<>();
 
 	public SelRecipeCon(JsonMap map, UniEntity ply, V3I pos){
 		super(map, ply, pos);
@@ -43,6 +50,28 @@ public class SelRecipeCon extends ContainerInterface {
 				for(String str : FclRecipe.RECIPES.keySet()) list.add(str);
 				com.set("cats", list);
 				com.set("cata", list.size());
+				SEND_TO_CLIENT.accept(com, player);
+			}
+		}
+		if(com.has("sync_res")){
+			if(client){
+				results.clear();
+				reskeys.clear();
+				for(TagCW res : com.getList("res")){
+					results.add(UniStack.createStack(res));
+					reskeys.add(res.getString("fcl-rec-key"));
+				}
+			}
+			else{
+				TagLW list = TagLW.create();
+				LinkedHashMap<IDL, ArrayList<FclRecipe>> res = FclRecipe.RECIPES.get(com.getString("c"));
+				for(Map.Entry<IDL, ArrayList<FclRecipe>> entry : res.entrySet()){
+					TagCW save = TagCW.create();
+					entry.getValue().get(0).output.save(save);
+					save.set("fcl-rec-key", entry.getKey().colon());
+					list.add(save);
+				}
+				com.set("res", list);
 				SEND_TO_CLIENT.accept(com, player);
 			}
 		}
