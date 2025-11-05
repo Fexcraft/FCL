102eefd45e3252ccdac22c5fba63341639c559ee
Ferdinand Calo <fexcalo@gmail.com>
2025-11-05 20:38:50 +0100

    Update UniUI.java

--- forge7/src/main/java/net/fexcraft/mod/uni/ui/UniUI.java
+++ forge7/src/main/java/net/fexcraft/mod/uni/ui/UniUI.java
@@ -1,4 +1,14 @@
 package net.fexcraft.mod.uni.ui;
 
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
 public class UniUI {
+
+	public UniUI(UserInterface ui, UIKey key, EntityPlayer player){
+
+	}
+
 }
