789093ecac2bebc806f4c07ca8c927eb54b3e4fe
Ferdinand Calo <fexcalo@gmail.com>
2025-11-05 21:14:33 +0100

    Create UUIField.java

---
+++ forge7/src/main/java/net/fexcraft/mod/uni/ui/UUIField.java
@@ -0,0 +1,96 @@
+package net.fexcraft.mod.uni.ui;
+
+import net.fexcraft.app.json.JsonMap;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiTextField;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class UUIField extends UIField {
+
+	protected GuiTextField field;
+
+	public UUIField(UserInterface ui, JsonMap map) throws Exception {
+		super(ui, map);
+		field = new GuiTextField(Minecraft.getMinecraft().fontRenderer, 0, 0, width, height){
+			@Override
+			public void writeText(String text){
+				//if(regex != null) text = text.replaceAll(regex, "");
+				if(number){
+					try{
+						text = df.format(Double.parseDouble(text));
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+				super.writeText(text);
+			}
+		};
+		field.setEnableBackgroundDrawing(background);
+		field.setTextColor(color);
+		field.setText(value);
+		field.setEnabled(enabled);
+		field.setVisible(visible);
+		field.setMaxStringLength(maxlength);
+	}
+
+	@Override
+	public void draw(Object gui, UIElement root, float ticks, int gl, int gt, int mx, int my){
+		if(!visible()) return;
+		field.xPosition = absolute ? x < 0 ? ui.screen_width + x : x : gl + x;
+		field.yPosition = absolute ? y < 0 ? ui.screen_height + y : y : gt + y;
+		field.drawTextBox();
+	}
+
+	@Override
+	public boolean visible(){
+		return field.getVisible();
+	}
+
+	@Override
+	public void visible(boolean bool){
+		field.setVisible(visible = bool);
+	}
+
+	@Override
+	public void enabled(boolean bool){
+		field.setEnabled(enabled = bool);
+	}
+
+	@Override
+	public boolean onclick(int mx, int my, int mb){
+		/*boolean bool = */field.mouseClicked(mx, my, mb);
+		if(field.isFocused()){
+			for(UIField uif : ui.fields.values()){
+				UUIField uuif = (UUIField) uif;
+				if(uuif.field.isFocused() && uuif.field != field){
+					uuif.field.setFocused(false);
+				}
+			}
+		}
+		return false;//bool;//TODO
+	}
+
+	@Override
+	public boolean keytyped(char c, int code){
+		return field.textboxKeyTyped(c, code);
+	}
+
+	@Override
+	public void text(String text){
+		field.setText(text);
+	}
+
+	@Override
+	public String text(){
+		return field.getText();
+	}
+
+	@Override
+	public void maxlength(int nl){
+		field.setMaxStringLength(nl);
+	}
+
+}
