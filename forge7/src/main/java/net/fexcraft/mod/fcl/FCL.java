a63bb38fcb258ccbd285b88ad7fd4520c1611721
Ferdinand Calo <fexcalo@gmail.com>
2025-11-05 20:36:36 +0100

    Update FCL.java

--- forge7/src/main/java/net/fexcraft/mod/fcl/FCL.java
+++ forge7/src/main/java/net/fexcraft/mod/fcl/FCL.java
@@ -3,17 +3,30 @@ package net.fexcraft.mod.fcl;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import io.netty.buffer.ByteBuf;
 import net.fexcraft.lib.common.utils.Formatter;
+import net.fexcraft.mod.fcl.mixint.CWProvider;
+import net.fexcraft.mod.fcl.mixint.EWProvider;
+import net.fexcraft.mod.fcl.mixint.SWProvider;
+import net.fexcraft.mod.fcl.packet.PacketHandler;
+import net.fexcraft.mod.fcl.util.OnPlayerClone;
 import net.fexcraft.mod.uni.*;
 import net.fexcraft.mod.uni.impl.*;
 import net.fexcraft.mod.uni.inv.*;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.tag.TagLW;
 import net.fexcraft.mod.uni.ui.*;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemLead;
@@ -21,10 +34,12 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.oredict.OreDictionary;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -35,6 +50,8 @@ public class FCL {
 	public static final Logger LOGGER = FMLLog.getLogger();
     public static final String MODID = "fcl";
     public static final String VERSION = "7.x";
+	@Mod.Instance
+	public static FCL INSTANCE;
 	public static UniFCL CONFIG;
 
 	@EventHandler
@@ -63,8 +80,8 @@ public class FCL {
 		ItemWrapper.SUPPLIER = item -> new IWI((Item)item);
 		StackWrapper.ITEM_TYPES.put(StackWrapper.IT_LEAD, item -> item instanceof ItemLead);
 		StackWrapper.ITEM_TYPES.put(StackWrapper.IT_FOOD, item -> item instanceof ItemFood);
-		/*UniInventory.IMPL = UniInventory12.class;
-		UniFluidTank.IMPL = UniFluidTank12.class;*/
+		UniInventory.IMPL = UniInventory7.class;
+		UniFluidTank.IMPL = UniFluidTank7.class;
 		if(EnvInfo.CLIENT){
 			/*UITab.IMPLEMENTATION = UUITab.class;
 			UIButton.IMPLEMENTATION = UUIButton.class;
@@ -74,14 +91,75 @@ public class FCL {
 			ContainerInterface.TRANSFORMAT = (str, objs) -> Formatter.format(net.minecraft.client.resources.I18n.format(str, objs));
 		}
 		CONFIG = new UniFCL(event.getModConfigurationDirectory());
+		UISlot.GETTERS.put("default", args -> new Slot((IInventory)args[0], (Integer)args[1], (Integer)args[2], (Integer)args[3]){
+			@Override
+			public boolean isItemValid(ItemStack stack){
+				if(args[0] instanceof UniInventory){
+					return ((UniInventory7)args[0]).valid((Integer)args[1], UniStack.getStack(stack));
+				}
+				return super.isItemValid(stack);
+			}
+		});
+		UniFCL.registerFCLUI(INSTANCE = this);
+		FclRecipe.VALIDATE = comp -> {
+			if(comp.tag) return OreDictionary.doesOreNameExist(comp.id);
+			else return !comp.stack.empty();
+		};
+		FclRecipe.GET_TAG_AS_LIST = (comp) -> {
+			ArrayList<StackWrapper> list = new ArrayList<>();
+			List<ItemStack> stacks = OreDictionary.getOres(comp.id);
+			for(ItemStack stack : stacks){
+				StackWrapper wrapper = UniStack.getStack(stack.copy());
+				wrapper.count(comp.amount);
+				list.add(wrapper);
+			}
+			return list;
+		};
+		MinecraftForge.EVENT_BUS.register(new OnPlayerClone());
     }
 
-	public static void bindTex(IDL tex){
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event) throws Exception{
+		StackWrapper.EMPTY = new SWI(new ItemStack(Blocks.air));
+		NetworkRegistry.INSTANCE.registerGuiHandler(INSTANCE, new GuiHandler());
+		if(UniFCL.EXAMPLE_RECIPES){
+			FclRecipe.newBuilder("recipe.fcl.testing").add(new ItemStack(Blocks.cobblestone, 4)).output(new ItemStack(Blocks.stone_stairs, 5)).register();
+			FclRecipe.newBuilder("recipe.fcl.testing").add("ingotIron", 9).output(new ItemStack(Blocks.iron_block, 1)).register();
+		}
+		//TODO crafting block recipe
+	}
 
+	@Mod.EventHandler
+	public void init(FMLServerStartingEvent event){
+		//TODO register FCL command
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		PacketHandler.init();
+		//
+		UniEntity.GETTER = ent -> ((EWProvider)ent).fcl_wrapper();
+		UniChunk.GETTER = ck -> ((CWProvider)ck).fcl_wrapper();
+		UniStack.GETTER = stk -> {
+			ItemStack stack = (ItemStack)(stk instanceof StackWrapper ? ((StackWrapper)stk).direct() : stk);
+			return ((SWProvider)(Object)stack).fcl_wrapper();
+		};
+		UniFCL.regTagPacketListener("fcl:ui", false, new UIPacketListener.Server());
+		if(event.getSide().isClient()){
+			UniFCL.regTagPacketListener("fcl:ui", true, new UIPacketListener.Client());
+		}
+	}
+
+	public static void bindTex(IDL tex){
+		net.minecraft.client.Minecraft.getMinecraft().getTextureManager().bindTexture(tex.local());
 	}
 
 	public static IDL requestServerFile(String lis, String loc){
 		return null;
+	}
+
+	public static void sendServerFile(EntityW player, String lis, String loc, byte[] tex){
+
 	}
 
 	public static void writeTag(ByteBuf buffer, TagCW com){
