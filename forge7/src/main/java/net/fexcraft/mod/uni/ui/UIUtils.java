bb182982e993a3b2c9a8b812e3d02d981e1f1367
Ferdinand Calo <fexcalo@gmail.com>
2025-11-05 20:38:28 +0100

    Create UIUtils.java

---
+++ forge7/src/main/java/net/fexcraft/mod/uni/ui/UIUtils.java
@@ -0,0 +1,56 @@
+package net.fexcraft.mod.uni.ui;
+
+import net.fexcraft.app.json.JsonHandler;
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fcl.FCL;
+import net.fexcraft.mod.uni.IDL;
+import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.UniReg;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class UIUtils {
+
+	public static JsonMap getMap(String mod, UIKey key){
+		String loc = null;
+		try{
+			IDL idl = UniReg.MENU_LOC.get(key);
+			loc = "data/" + idl.space() + "/" + idl.path();
+			return JsonHandler.parse(UniReg.getInst(mod).getClass().getClassLoader().getResourceAsStream(loc));
+		}
+		catch(Exception e){
+			FCL.LOGGER.info("ui-loc [" + key.key + "]: " + loc);
+			e.printStackTrace();
+			return new JsonMap();
+		}
+	}
+
+	public static Object getClient(String mod, int id, EntityPlayer player, int x, int y, int z){
+		UIKey key = UIKey.find(mod, id);
+		JsonMap map = getMap(mod, key);
+		try{
+			ContainerInterface con = UniReg.MENU.get(key).getConstructor(JsonMap.class, UniEntity.class, V3I.class).newInstance(map, UniEntity.get(player), new V3I(x, y, z));
+			return new UniUI(UniReg.GUI.get(key).getConstructor(JsonMap.class, ContainerInterface.class).newInstance(map, con), key, player);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static Object getServer(String mod, int id, EntityPlayer player, int x, int y, int z){
+		UIKey key = UIKey.find(mod, id);
+		JsonMap map = getMap(mod, key);
+		try{
+			return new UniCon(UniReg.MENU.get(key).getConstructor(JsonMap.class, UniEntity.class, V3I.class).newInstance(map, UniEntity.get(player), new V3I(x, y, z)), key, player);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
