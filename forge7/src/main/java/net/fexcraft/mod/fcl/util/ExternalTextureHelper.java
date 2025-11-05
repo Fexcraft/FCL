456fc1fe63fe4d0144f25cb1896a68878df9621c
Ferdinand Calo <fexcalo@gmail.com>
2025-11-05 20:37:07 +0100

    Create ExternalTextureHelper.java

---
+++ forge7/src/main/java/net/fexcraft/mod/fcl/util/ExternalTextureHelper.java
@@ -0,0 +1,148 @@
+package net.fexcraft.mod.fcl.util;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.fexcraft.mod.fcl.FCL;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.ITextureObject;
+import net.minecraft.client.renderer.texture.SimpleTexture;
+import net.minecraft.client.renderer.texture.TextureUtil;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.util.ResourceLocation;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class ExternalTextureHelper {
+	
+	private static final Map<String, ResourceLocation> map = new HashMap<>();
+	
+	public static ResourceLocation get(String s){
+		if(map.containsKey(s)) return map.get(s);
+		ResourceLocation texture = new ResourceLocation("fcl-url", s);
+		ITextureObject object = Minecraft.getMinecraft().renderEngine.getTexture(texture);
+        if(object == null){
+        	SimplerThreadImageDownloader thid = new SimplerThreadImageDownloader(s, texture);
+        	Minecraft.getMinecraft().renderEngine.loadTexture(texture, (object = thid));
+        }
+        map.put(s, texture);
+        return texture;
+	}
+
+	public static ResourceLocation get(String s, byte[] arr){
+		if(map.containsKey(s)){
+			if(arr != null) map.remove(s);
+			else return map.get(s);
+		}
+		if(arr == null) return new ResourceLocation(s);
+		ResourceLocation tex = new ResourceLocation(s);
+		Minecraft.getMinecraft().renderEngine.deleteTexture(tex);
+		Minecraft.getMinecraft().renderEngine.loadTexture(tex, new BufferedTextureLoader(tex, arr));
+		map.put(s, tex);
+		return tex;
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public static class SimplerThreadImageDownloader extends SimpleTexture {
+		
+	    private static final AtomicInteger THRID = new AtomicInteger(0);
+	    private final String url;
+	    private BufferedImage image;
+	    private Thread thread;
+	    private boolean uploaded;
+
+	    public SimplerThreadImageDownloader(String imgurl, ResourceLocation resloc) {
+	        super(resloc);
+	        url = imgurl;
+	    }
+
+	    private void checkIfUploaded(){
+	    	if(uploaded || image == null) return;
+            if(textureLocation != null) deleteGlTexture();
+            TextureUtil.uploadTextureImage(super.getGlTextureId(), image);
+            uploaded = true;
+	    }
+
+	    @Override
+	    public int getGlTextureId(){
+	        checkIfUploaded();
+	        return super.getGlTextureId();
+	    }
+
+	    @Override
+	    public void loadTexture(IResourceManager resman) throws IOException {
+	        if(thread != null) return;
+			thread = new Thread(() -> {
+				HttpURLConnection conn = null;
+				FCL.LOGGER.info("Attempting download of texture '" + url + "'.");
+				try{
+					conn = (HttpURLConnection)new URL(url).openConnection(Minecraft.getMinecraft().getProxy());
+					conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+					conn.setDoInput(true);
+					conn.setDoOutput(false);
+					conn.connect();
+					if(conn.getResponseCode() == 200){
+						image = ImageIO.read(conn.getInputStream());
+						return;
+					}
+					else{
+						FCL.LOGGER.info("Received response code '" + conn.getResponseCode() + "'.");
+					}
+				}
+				catch(Exception e) {
+					FCL.LOGGER.info("Errors during download of texture.");
+					e.printStackTrace();
+					return;
+				}
+				finally{
+					if(conn != null) conn.disconnect();
+				}
+			});
+			thread.setName("FCL TexDL #" + THRID.incrementAndGet());
+			thread.setDaemon(true);
+			thread.start();
+	    }
+	    
+	}
+
+	@SideOnly(Side.CLIENT)
+	public static class BufferedTextureLoader extends SimpleTexture {
+
+		private BufferedImage image;
+		private byte[] array;
+		private boolean uploaded;
+
+		public BufferedTextureLoader(ResourceLocation resloc, byte[] arr) {
+			super(resloc);
+			array = arr;
+		}
+
+		private void checkIfUploaded(){
+			if(uploaded || image == null) return;
+			if(textureLocation != null) deleteGlTexture();
+			TextureUtil.uploadTextureImage(super.getGlTextureId(), image);
+			uploaded = true;
+		}
+
+		@Override
+		public int getGlTextureId(){
+			checkIfUploaded();
+			return super.getGlTextureId();
+		}
+
+		@Override
+		public void loadTexture(IResourceManager resman) throws IOException {
+			image = ImageIO.read(new ByteArrayInputStream(array));
+		}
+
+	}
+	
+}
