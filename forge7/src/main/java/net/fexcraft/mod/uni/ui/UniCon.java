edf13d62e4a0597b5c17ffc3a343c2f008a6a76d
Ferdinand Calo <fexcalo@gmail.com>
2025-11-05 20:38:45 +0100

    Update UniCon.java

--- forge7/src/main/java/net/fexcraft/mod/uni/ui/UniCon.java
+++ forge7/src/main/java/net/fexcraft/mod/uni/ui/UniCon.java
@@ -1,4 +1,26 @@
 package net.fexcraft.mod.uni.ui;
 
-public class UniCon {
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class UniCon extends Container {
+
+	protected ContainerInterface con;
+
+	public UniCon(ContainerInterface con, UIKey key, EntityPlayer player){
+
+	}
+
+	@Override
+	public boolean canInteractWith(EntityPlayer p_75145_1_){
+		return false;
+	}
+
+	public ContainerInterface container(){
+		return con;
+	}
+
 }
