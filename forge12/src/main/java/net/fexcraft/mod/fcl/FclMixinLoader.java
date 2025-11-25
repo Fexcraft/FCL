bb74e44a0d1b82ca436ac3d35cd739142fd6d21c
Ferdinand Calo <fexcalo@gmail.com>
2025-11-25 20:54:25 +0100

    1.12 Mixins & Capability Removal

---
+++ forge12/src/main/java/net/fexcraft/mod/fcl/FclMixinLoader.java
@@ -0,0 +1,49 @@
+package net.fexcraft.mod.fcl;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import scala.actors.threadpool.Arrays;
+import zone.rong.mixinbooter.IEarlyMixinLoader;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Map;
+
+@IFMLLoadingPlugin.Name("FCL Mixin Loader")
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+@IFMLLoadingPlugin.TransformerExclusions("net.fexcraft.mod.fcl")
+@IFMLLoadingPlugin.SortingIndex(1996)
+@IFMLLoadingPlugin.DependsOn("zone.rong.mixinbooter.MixinBooterPlugin")
+public class FclMixinLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
+
+	@Override
+	public String[] getASMTransformerClass(){
+		return new String[0];
+	}
+
+	@Override
+	public String getModContainerClass(){
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public String getSetupClass(){
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data){
+
+	}
+
+	@Override
+	public String getAccessTransformerClass(){
+		return null;
+	}
+
+	@Override
+	public List<String> getMixinConfigs(){
+		return Arrays.asList(new String[]{"fcl.mixins.json"});
+	}
+
+}
