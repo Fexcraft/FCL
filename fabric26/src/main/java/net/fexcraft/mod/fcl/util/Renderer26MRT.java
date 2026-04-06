7248c16915e2d3e648deae9354f2f95bfa2e1e3c
Ferdinand Calo <fexcalo@gmail.com>
2026-04-06 16:55:03 +0200

    Update Renderer26MRT.java

--- fabric26/src/main/java/net/fexcraft/mod/fcl/util/Renderer26MRT.java
+++ fabric26/src/main/java/net/fexcraft/mod/fcl/util/Renderer26MRT.java
@@ -8,6 +8,7 @@ import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.common.math.TexturedPolygon;
 import net.fexcraft.lib.common.math.TexturedVertex;
 import net.fexcraft.lib.tmt.ModelRendererTurbo;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import org.joml.*;
@@ -17,17 +18,17 @@ import org.joml.*;
  *
  * @author Ferdinand Calo' (FEX___96)
  */
-public class Renderer21MRT extends ModelRendererTurbo.Renderer {
+public class Renderer26MRT extends ModelRendererTurbo.Renderer {
 
 	public static final Vector3f AY = Renderer26.AY;
 	public static final Vector3f AX = Renderer26.AX;
 	public static final Vector3f AZ = Renderer26.AZ;
-	public static Renderer21MRT REN_IN = new Renderer21MRT();
 	//
 	private static int color = 0xffffffff;
 	//
-	public static PoseStack pose;
-	private static VertexConsumer cons;
+	public static RenderType type;
+	public static PoseStack stack;
+	public static SubmitNodeCollector noco;
 	protected static int overlay = OverlayTexture.NO_OVERLAY;
 	public static int light;
 
@@ -40,11 +41,11 @@ public class Renderer21MRT extends ModelRendererTurbo.Renderer {
 	}
 
 	public static void rotateDeg(float by, Vector3f axe){
-		pose.mulPose(new Quaternionf().rotateAxis(Static.toRadians(by), axe));
+		stack.mulPose(new Quaternionf().rotateAxis(Static.toRadians(by), axe));
 	}
 
 	public static void rotateRad(float by, Vector3f axe){
-		pose.mulPose(new Quaternionf().rotateAxis(by, axe));
+		stack.mulPose(new Quaternionf().rotateAxis(by, axe));
 	}
 
 	public static void rotateDeg(PoseStack pose, float by, Vector3f axe){
@@ -56,62 +57,64 @@ public class Renderer21MRT extends ModelRendererTurbo.Renderer {
 	}
 
 	public static void pushPose(){
-		pose.pushPose();
+		stack.pushPose();
 	}
 
 	public static void popPose(){
-		pose.popPose();
+		stack.popPose();
 	}
 
-	public void render(ModelRendererTurbo turbo, float scale){}
-
-	public void render(ModelRendererTurbo turbo, float scale, PoseStack.Pose pose, RenderType type, VertexConsumer vcon, int li){
-		if(!turbo.showModel) return;
-		light = li;
-		cons = vcon;
-		Matrix4f verma = pose.pose();
-		Matrix3f norma = pose.normal();
-		for(TexturedPolygon poli : turbo.getFaces()){
-			if(type.mode() == VertexFormat.Mode.QUADS){
-				for(TexturedVertex vert : poli.getVertices()){
-					fillVert(poli, verma, norma, vert, scale);
-				}
-				if(poli.getVertices().length < 4){
-					fillVert(poli, verma, norma, poli.getVertices()[2], scale);
-				}
-			}
-			else{
-				if(poli.getVertices().length == 4){
-					fillVert(poli, verma, norma, poli.getVertices()[0], scale);
-					fillVert(poli, verma, norma, poli.getVertices()[1], scale);
-					fillVert(poli, verma, norma, poli.getVertices()[2], scale);
-					fillVert(poli, verma, norma, poli.getVertices()[0], scale);
-					fillVert(poli, verma, norma, poli.getVertices()[2], scale);
-					fillVert(poli, verma, norma, poli.getVertices()[3], scale);
-				}
-				else{
-					for(TexturedVertex vert : poli.getVertices()){
-						fillVert(poli, verma, norma, vert, scale);
-					}
-				}
-			}
-		}
+	public static void set(PoseStack pstack, RenderType rtype, SubmitNodeCollector node, int lc){
+		stack = pstack;
+		type = rtype;
+		noco = node;
+		light = lc;
 	}
 
-	public void transform(ModelRendererTurbo turbo, float scale){
+	public void render(ModelRendererTurbo turbo, float scale){
 		if(!turbo.showModel) return;
-		pose.pushPose();
-		pose.translate(turbo.rotationPointX * scale, turbo.rotationPointY * scale, turbo.rotationPointZ * scale);
+		stack.pushPose();
+		stack.translate(turbo.rotationPointX * scale, turbo.rotationPointY * scale, turbo.rotationPointZ * scale);
 		if(turbo.rotationAngleX != 0.0F || turbo.rotationAngleY != 0.0F || turbo.rotationAngleZ != 0.0F){
-			pose.mulPose(new Quaternionf()
+			stack.mulPose(new Quaternionf()
 				.rotateAxis(Static.toRadians(turbo.rotationAngleY), AY)
 				.rotateAxis(Static.toRadians(turbo.rotationAngleX), AX)
 				.rotateAxis(Static.toRadians(turbo.rotationAngleZ), AZ)
 			);
 		}
+		noco.submitCustomGeometry(stack, type, (pose, cons) -> {
+			Matrix4f verma = pose.pose();
+			Matrix3f norma = pose.normal();
+			for(TexturedPolygon poli : turbo.getFaces()){
+				if(type.mode() == VertexFormat.Mode.QUADS){
+					for(TexturedVertex vert : poli.getVertices()){
+						fillVert(cons, poli, verma, norma, vert, scale);
+					}
+					if(poli.getVertices().length < 4){
+						fillVert(cons, poli, verma, norma, poli.getVertices()[2], scale);
+					}
+				}
+				else{
+					if(poli.getVertices().length == 4){
+						fillVert(cons, poli, verma, norma, poli.getVertices()[0], scale);
+						fillVert(cons, poli, verma, norma, poli.getVertices()[1], scale);
+						fillVert(cons, poli, verma, norma, poli.getVertices()[2], scale);
+						fillVert(cons, poli, verma, norma, poli.getVertices()[0], scale);
+						fillVert(cons, poli, verma, norma, poli.getVertices()[2], scale);
+						fillVert(cons, poli, verma, norma, poli.getVertices()[3], scale);
+					}
+					else{
+						for(TexturedVertex vert : poli.getVertices()){
+							fillVert(cons, poli, verma, norma, vert, scale);
+						}
+					}
+				}
+			}
+		});
+		stack.popPose();
 	}
 
-	private void fillVert(TexturedPolygon poly, Matrix4f verma, Matrix3f norma, TexturedVertex vert, float scale){
+	private void fillVert(VertexConsumer cons, TexturedPolygon poly, Matrix4f verma, Matrix3f norma, TexturedVertex vert, float scale){
 		Vector4f vec = verma.transform(new Vector4f(vert.vector.x * scale, vert.vector.y * scale, vert.vector.z * scale, 1));
 		if(poly.getNormals().length == 0) poly.genIfMissingNormals();
 		Vector3f norm = norma.transform(new Vector3f(poly.getNormals()[0], poly.getNormals()[1], poly.getNormals()[2]));
