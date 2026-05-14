9e3827abca257740feb03239871b9a5f456a2a8c
Ferdinand Calo <fexcalo@gmail.com>
2026-05-14 12:22:43 +0200

    Update GLObject.java

--- common/src/net/fexcraft/lib/frl/GLObject.java
+++ common/src/net/fexcraft/lib/frl/GLObject.java
@@ -3,14 +3,14 @@ package net.fexcraft.lib.frl;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class GLObject extends GLO<GLObject> {
+public class GLObject extends GLO {
 
 	public boolean textured = true;
 
 	@Override
-	public void copy(GLObject from, boolean full){
+	public void copy(GLO from, boolean full){
 		super.copy(from, full);
-		textured = from.textured;
+		textured = ((GLObject)from).textured;
 	}
 
 }
