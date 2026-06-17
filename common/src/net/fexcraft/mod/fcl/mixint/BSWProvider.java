eb03a46cfe65f7576e96c576bcec54b4cf9826c2
Ferdinand Calo <fexcalo@gmail.com>
2026-06-17 23:22:19 +0200

    Create BSWProvider.java

---
+++ common/src/net/fexcraft/mod/fcl/mixint/BSWProvider.java
@@ -0,0 +1,12 @@
+package net.fexcraft.mod.fcl.mixint;
+
+import net.fexcraft.mod.uni.world.StateWrapper;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public interface BSWProvider {
+
+	StateWrapper fcl_wrapper();
+
+}
