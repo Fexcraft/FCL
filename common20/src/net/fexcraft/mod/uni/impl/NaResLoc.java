b4a3c6b865d17acf988061fa67ec2bf1030e1866
Ferdinand Calo <fexcalo@gmail.com>
2026-06-25 07:28:02 +0200

    Update NaResLoc.java

--- common20/src/net/fexcraft/mod/uni/impl/NaResLoc.java
+++ common20/src/net/fexcraft/mod/uni/impl/NaResLoc.java
@@ -6,30 +6,34 @@ import net.minecraft.resources.ResourceLocation;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class NaResLoc extends NamedResourceLocation implements IDL {
+public class NaResLoc extends ResourceLocation implements IDL {
+
+	private String name;
 
 	public NaResLoc(String name, String domain, String path){
-		super(name, domain, path);
-	}
-
-	public NaResLoc(String name, ResourceLocation rs){
-		super(name, rs);
+		super(domain, path);
+		if(name == null) name = "Unnamed";
+		this.name = name;
 	}
 
 	public NaResLoc(String onestring){
-		super(onestring);
+		super(onestring.contains(";") ? onestring.split(";")[1] : onestring);
+		name = onestring.contains(";") ? onestring.split(";")[0] : "Unnamed";
 	}
 
+	@Override
 	public String space(){
 		return getNamespace();
 	}
 
+	@Override
 	public String id(){
 		return getPath();
 	}
 
+	@Override
 	public String name(){
-		return getName();
+		return name;
 	}
 
 }
