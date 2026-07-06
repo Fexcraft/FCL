6bedc74c1b120c5baac7c583c8506f1c69805a71
Ferdinand Calo <fexcalo@gmail.com>
2026-07-06 10:57:06 +0200

    Update FclMixinLoader.java

--- forge12/src/main/java/net/fexcraft/mod/fcl/FclMixinLoader.java
+++ forge12/src/main/java/net/fexcraft/mod/fcl/FclMixinLoader.java
@@ -1,19 +1,20 @@
 package net.fexcraft.mod.fcl;
 
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
-import zone.rong.mixinbooter.IEarlyMixinLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixins;
 
 import javax.annotation.Nullable;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 @IFMLLoadingPlugin.Name("FCL Mixin Loader")
 @IFMLLoadingPlugin.MCVersion("1.12.2")
 @IFMLLoadingPlugin.TransformerExclusions("net.fexcraft.mod.fcl")
 @IFMLLoadingPlugin.SortingIndex(1996)
-@IFMLLoadingPlugin.DependsOn("zone.rong.mixinbooter.MixinBooterPlugin")
-public class FclMixinLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
+public class FclMixinLoader implements IFMLLoadingPlugin {
+
+	public static final Logger LOGGER = LogManager.getLogger("FCL-MIXIN");
 
 	@Override
 	public String[] getASMTransformerClass(){
@@ -33,17 +34,13 @@ public class FclMixinLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
 	@Override
 	public void injectData(Map<String, Object> data){
-
+		LOGGER.info("Inserting FCL mixins.json");
+		Mixins.addConfiguration("fcl.mixins.json");
 	}
 
 	@Override
 	public String getAccessTransformerClass(){
 		return null;
-	}
-
-	@Override
-	public List<String> getMixinConfigs(){
-		return Arrays.asList(new String[]{"fcl.mixins.json"});
 	}
 
 }
