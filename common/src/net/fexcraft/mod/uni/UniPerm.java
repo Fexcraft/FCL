f7fd78689ac33cb99793209257be8782c224294b
Ferdinand Calo <fexcalo@gmail.com>
2026-08-13 20:01:43 +0200

    Update UniPerm.java

--- common/src/net/fexcraft/mod/uni/UniPerm.java
+++ common/src/net/fexcraft/mod/uni/UniPerm.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.uni;
 
+import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.uni.world.EntityW;
 import net.fexcraft.mod.uni.world.WrapperHolder;
 
@@ -8,10 +9,10 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public abstract class UniPerm {
+public class UniPerm {
 
 	protected static ConcurrentHashMap<String, Integer> PERMS = new ConcurrentHashMap<>();
-	public static UniPerm INSTANCE = new DefUniPerm();
+	public static UniPerm INSTANCE = new UniPerm();
 
 	public static void register(String perm){
 		register(perm, 4);
@@ -23,27 +24,39 @@ public abstract class UniPerm {
 		INSTANCE.register0(perm, lvl);
 	}
 
-	protected abstract void register0(String perm, int lvl);
+	public void register0(String perm, int lvl){}
 
 	public static boolean has(EntityW ent, String perm){
 		return INSTANCE.has0(ent, perm);
 	}
 
-	protected abstract boolean has0(EntityW ent, String perm);
+	public boolean has0(EntityW ent, String perm){
+		int lvl = PERMS.getOrDefault(perm, 4);
+		return lvl == 0 || WrapperHolder.isSinglePlayer() || WrapperHolder.isOp(ent, lvl);
+	}
 
-	public static class DefUniPerm extends UniPerm {
+	public static boolean can_interact(EntityW ent, V3I pos){
+		return INSTANCE.can_interact0(ent, pos);
+	}
 
-		@Override
-		protected void register0(String perm, int lvl){
-			//
-		}
+	public boolean can_interact0(EntityW ent, V3I pos){
+		return true;
+	}
 
-		@Override
-		protected boolean has0(EntityW ent, String perm){
-			int lvl = PERMS.getOrDefault(perm, 4);
-			return lvl == 0 || WrapperHolder.isSinglePlayer() || WrapperHolder.isOp(ent, lvl);
-		}
+	public static boolean can_break(EntityW ent, V3I pos){
+		return INSTANCE.can_break0(ent, pos);
+	}
 
+	public boolean can_break0(EntityW ent, V3I pos){
+		return true;
+	}
+
+	public static boolean can_place(EntityW ent, V3I pos){
+		return INSTANCE.can_place0(ent, pos);
+	}
+
+	public boolean can_place0(EntityW ent, V3I pos){
+		return true;
 	}
 
 }
