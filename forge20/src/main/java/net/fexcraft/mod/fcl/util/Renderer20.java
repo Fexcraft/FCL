818a0df1f91b8b65d9284db13dd0940c58eb7660
Ferdinand Calo <fexcalo@gmail.com>
2026-09-10 21:16:49 +0200

    Update Renderer20.java

--- forge20/src/main/java/net/fexcraft/mod/fcl/util/Renderer20.java
+++ forge20/src/main/java/net/fexcraft/mod/fcl/util/Renderer20.java
@@ -144,7 +144,7 @@ public class Renderer20 extends Renderer {
 
 	private void fillVert(Matrix4f verma, Matrix3f norma, Vertex vert){
 		Vector4f vec = verma.transform(new Vector4f(vert.vector.x, vert.vector.y, vert.vector.z, 1.0F));
-		Vector3f norm = norma.transform(vert.norm == null ? NULLVEC : new Vector3f(vert.norm.x, vert.norm.y, vert.norm.z));
+		Vector3f norm = norma.transform(vert.norm == null ? new Vector3f(1, 1, 1) : new Vector3f(vert.norm.x, vert.norm.y, vert.norm.z));
 		cons.vertex(vec.x, vec.y, vec.z, color[0], color[1], color[2], alpha, vert.u, vert.v, overlay, light, norm.x, norm.y, norm.z);
 	}
 
