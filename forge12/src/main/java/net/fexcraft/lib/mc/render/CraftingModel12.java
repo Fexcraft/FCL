a5ec4709e83d6d2d15269bdb20f65c6b1ecdcad0
Ferdinand Calo <fexcalo@gmail.com>
2026-09-12 13:24:23 +0200

    Update CraftingModel12.java

--- forge12/src/main/java/net/fexcraft/lib/mc/render/CraftingModel12.java
+++ forge12/src/main/java/net/fexcraft/lib/mc/render/CraftingModel12.java
@@ -1,27 +1,30 @@
 package net.fexcraft.lib.mc.render;
 
+import net.fexcraft.lib.frl.CompactModel;
+import net.fexcraft.lib.frl.Polyhedron;
 import net.fexcraft.lib.mc.api.registry.fModel;
-import net.fexcraft.lib.tmt.ModelRendererTurbo;
-import net.fexcraft.mod.fcl.util.CraftingModel;
+import net.fexcraft.mod.fcl.UniFCL;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 @fModel(registryname = "fcl:models/block/crafting")
-public class CraftingModel12 extends CraftingModel implements FCLBlockModel {
+public class CraftingModel12 implements FCLBlockModel {
 
 	public CraftingModel12(){ super(); }
 
 	@Override
-	public Collection<ModelRendererTurbo> getPolygons(IBlockState state, EnumFacing side, Map<String, String> arguments, long rand){
-		ArrayList<ModelRendererTurbo> list = new ArrayList<>();
-		for(ArrayList<ModelRendererTurbo> tlist : groups) list.addAll(tlist);
+	public Collection<Polyhedron> getPolygons(IBlockState state, EnumFacing side, Map<String, String> arguments, long rand){
+		if(UniFCL.CRAFTING_MODEL == null) return Collections.emptyList();
+		ArrayList<Polyhedron> list = new ArrayList<>();
+		for(CompactModel.CompactGroup group : UniFCL.CRAFTING_MODEL.groups.values()) list.addAll(group.polyhedrons);
 		return list;
 	}
 
