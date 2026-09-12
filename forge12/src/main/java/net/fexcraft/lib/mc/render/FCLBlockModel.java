1ace1d99b69920af3522486c9e8d84ce6e16982b
Ferdinand Calo <fexcalo@gmail.com>
2026-09-12 13:24:26 +0200

    Update FCLBlockModel.java

--- forge12/src/main/java/net/fexcraft/lib/mc/render/FCLBlockModel.java
+++ forge12/src/main/java/net/fexcraft/lib/mc/render/FCLBlockModel.java
@@ -3,14 +3,14 @@ package net.fexcraft.lib.mc.render;
 import java.util.Collection;
 import java.util.Map;
 
-import net.fexcraft.lib.tmt.ModelRendererTurbo;
+import net.fexcraft.lib.frl.Polyhedron;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 
 public interface FCLBlockModel {
 	
-	public Collection<ModelRendererTurbo> getPolygons(IBlockState state, EnumFacing side, Map<String, String> arguments, long rand);
+	public Collection<Polyhedron> getPolygons(IBlockState state, EnumFacing side, Map<String, String> arguments, long rand);
 
 	public default Collection<ResourceLocation> getTextures(Map<String, String> customdata){ return null; }
 
